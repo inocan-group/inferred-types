c80005d957669f8b2baa62c5850c613bffbb9cb7
Ken Snyder <ken@ken.net>
2022-06-14 13:30:24 -0700

    feat: added variant utilities for adding a function with key/value pairs

--- src/utility/createFnWithProps.ts
+++ src/utility/createFnWithProps.ts
@@ -1,3 +1,4 @@
+import { Narrowable } from "../types/Narrowable";
 import { keys } from "./keys";
 
 export function createFnWithProps<F extends Function, P extends {}>(fn: F, props: P) {
@@ -9,4 +10,28 @@ export function createFnWithProps<F extends Function, P extends {}>(fn: F, props
     }
     return combined;
   })() as unknown as F & P;
+}
+
+/**
+ * Adds a dictionary of key/value pairs to a function.
+ */
+export function fnWithProps<A extends any[], R extends any, P extends {}>(fn: ((...args: A) => R), props: P) {
+  // eslint-disable-next-line prefer-const
+  let combined: any = fn;
+  for (const prop of keys(props)) {
+    combined[prop] = props[prop];
+  }
+  return combined as ((...args: A) => R) & P;
+}
+
+/**
+ * Adds read-only (and narrowly typed) key/value pairs to a function
+ */
+export function readonlyFnWithProps<A extends any[], R extends any, N extends Narrowable, P extends Record<keyof P, N>>(fn: ((...args: A) => R), props: P) {
+  // eslint-disable-next-line prefer-const
+  let combined: any = fn;
+  for (const prop of keys(props)) {
+    combined[prop] = props[prop];
+  }
+  return combined as ((...args: A) => R) & Readonly<P>;
 }
