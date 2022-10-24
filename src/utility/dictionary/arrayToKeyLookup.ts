529ee25aff1e6eb460a73193bd1eae60c0e574e8
Ken Snyder <ken@ken.net>
2022-10-23 21:57:40 -0700

    feature: added dictArr API

--- src/utility/dictionary/arrayToKeyLookup.ts
+++ src/utility/dictionary/arrayToKeyLookup.ts
@@ -1,4 +1,5 @@
-import { RequiredKeys } from "src/types/dictionary";
+import { Keys, Narrowable, UniqueForProp } from "src/types";
+import { OptionalKeys, RequiredKeys } from "src/types/dictionary";
 
 /**
  * Passing in an array of strings, you are passed back a dictionary with
@@ -20,23 +21,72 @@ export function arrayToKeyLookup<T extends readonly string[]>(...keys: T): Recor
   return obj;
 }
 
+export interface Uniqueness<T> {
+  /** boolean flag to indicate whether the property was unique across all records */
+  isUnique: boolean;
+  /** the overall number of records which contained the property */
+  size: number;
+  /** specifies if undefined values were encountered for this property */
+  includedUndefined: boolean;
+  /** the unique values for the property across all records */
+  values: readonly T[];
+}
+
+export type DictArrApi<O extends Record<string, Narrowable>, A extends readonly O[]> = {
+  length: number;
+  toLookup<PL extends RequiredKeys<O, string> & keyof O & string>(
+    prop: PL
+  ): Record<UniqueForProp<A, PL>, O>;
+  sum<PS extends RequiredKeys<O, number> | OptionalKeys<O, number>>(prop: PS): number;
+  count<PC extends OptionalKeys<O>>(prop: PC): number;
+  unique<PU extends Keys<O> & keyof O>(prop: PU): Uniqueness<O[PU]>;
+};
+
 /**
  * converts an array of objects to a dictionary with keys formed from a given property
  * of the object and the value being the object itself.
  */
-export const dictArr = <O extends Record<string, any>, T extends readonly O[]>(...dicts: T) => ({
-  toLookup: <P extends keyof O>(prop: P) => {
-    // const
-  },
-});
+export const dictArr = <T extends Record<string, Narrowable>>(...dicts: readonly T[]) => {
+  // build API
+  const api: DictArrApi<T, typeof dicts> = {
+    length: dicts.length,
+    toLookup: (prop) => {
+      // type D = typeof dicts;
+      // type TA = UniqueForProp<D, typeof prop>;
+      let dict: Record<string, T> = {};
+      for (const obj of dicts) {
+        const key = obj[prop] as string;
+        dict = { ...dict, [key]: obj };
+      }
+      return dict;
+    },
+    /**
+     * The unique values of a given property
+     */
+    unique: (prop) => {
+      type P = typeof prop;
+      const v = new Set<O[P]>();
+      dicts.forEach((i) => v.add(i[prop]));
+      const size = dictArr(...dicts).count(prop as any);
+      const values = Array.from(v);
+      const u: Uniqueness<T[P]> = {
+        isUnique: values.includes(undefined as any) ? size === v.size - 1 : size === v.size,
+        includedUndefined: values.includes(undefined as any) ? true : false,
+        size,
+        values,
+      };
+      return u;
+    },
+    sum: (prop) => {
+      return dicts.reduce((acc, obj) => (prop in obj ? acc + (obj[prop] as number) : acc), 0);
+    },
+    count: (prop) => {
+      return dicts.reduce(
+        (acc, obj) => (prop in obj && typeof obj[prop] !== "undefined" ? acc + 1 : acc),
+        0
+      );
+    },
+  };
 
-const arr = [
-  { name: "foo", title: "foo" },
-  { name: "bar", title: "bar" },
-] as const;
-type T1 = { name: string; title: string; color?: string; value: number };
-type T2 = RequiredKeys<T1>;
-const d = dictArr<T1>([
-  { name: "foo", title: "foo", value: 320 },
-  { name: "bar", title: "bar", value: 654 },
-]).toLookup("");
+  return api;
+};
---
+++ tests/lists/dictArr.test.ts
@@ -0,0 +1,108 @@
+import { describe, expect, it } from "vitest";
+import type { Expect, Equal } from "@type-challenges/utils";
+import { dictArr, keys, literal } from "src/utility";
+
+type O = {
+  id: number;
+  name: string;
+  title: string;
+  cost?: number;
+  color?: string;
+};
+
+const genericArr: O[] = [
+  { id: 1, name: "foo", title: "one", cost: 15 },
+  { id: 2, name: "bar", title: "one" },
+  { id: 3, name: "baz", title: "two", cost: 45, color: "green" },
+];
+
+const literalArr = [
+  literal({ id: 1, name: "foo", title: "one", cost: 15 }),
+  literal({ id: 2, name: "bar", title: "one" }),
+  literal({ id: 3, name: "baz", title: "two", cost: 45, color: "green" }),
+];
+
+const wideArr = [
+  { id: 1, name: "foo", title: "one", cost: 15 },
+  { id: 2, name: "bar", title: "one" },
+  { id: 3, name: "baz", title: "two", cost: 45, color: "green" },
+];
+
+describe("dictArr() utility API", () => {
+  it("returns an API when passed the array", () => {
+    const r = dictArr(genericArr);
+    type R = typeof r;
+    // runtime checks
+    expect(typeof r.toLookup).contains("function");
+    expect(typeof r.unique).contains("function");
+    expect(typeof r.count).contains("function");
+    expect(typeof r.sum).contains("function");
+    expect(typeof r.length).contains("number");
+
+    // type checks
+    type ToLookupParam = Parameters<R["toLookup"]>[0];
+    type UniqueParam = Parameters<R["unique"]>[0];
+    type CountParam = Parameters<R["count"]>[0];
+    type SumParam = Parameters<R["sum"]>[0];
+
+    type cases = [
+      Expect<Equal<ToLookupParam, "name" | "title">>,
+      Expect<Equal<UniqueParam, "name" | "title" | "cost" | "color">>,
+      Expect<Equal<CountParam, "cost" | "color">>,
+      Expect<Equal<SumParam, "cost">>
+    ];
+    const cases: cases = [true, true, true, true];
+  });
+
+  it("toLookup() creates type-strong lookup", () => {
+    const g = dictArr(...genericArr);
+    const l = dictArr(...literalArr);
+    const pl = dictArr(...wideArr);
+    const byNameG = g.toLookup("name");
+    const byNameL = l.toLookup("name");
+    const byNamePL = pl.toLookup("name");
+    const kg = keys(byNameG);
+    const kl = keys(byNameL);
+    const kpl = keys(byNamePL);
+    expect(kg).contains("foo");
+    expect(kg).contains("bar");
+    expect(kg).contains("baz");
+
+    expect(kl).contains("foo");
+    expect(kl).contains("bar");
+    expect(kl).contains("baz");
+
+    expect(kpl).contains("foo");
+    expect(kpl).contains("bar");
+    expect(kpl).contains("baz");
+
+    const fooG = genericArr.find((i) => i.name === "foo");
+    expect(byNameG.foo).toEqual(fooG);
+
+    const fooL = literalArr.find((i) => i.name === "foo");
+    expect(byNameL.foo).toEqual(fooL);
+  });
+
+  it("summarization works", () => {
+    const r = dictArr(...genericArr);
+    expect(r.sum("cost")).toBe(60);
+  });
+
+  it("counting of non-required props works", () => {
+    const r = dictArr(...genericArr);
+    expect(r.count("color")).toBe(1);
+    expect(r.count("cost")).toBe(2);
+  });
+
+  it("uniqueness", () => {
+    const r = dictArr(...genericArr);
+    const color = r.unique("color");
+    const title = r.unique("title");
+
+    expect(color.isUnique).toBeTruthy();
+    expect(color.includedUndefined).toBeTruthy();
+    expect(color.values).toEqual([undefined, "green"]);
+    expect(title.isUnique).toBeFalsy();
+    expect(title.values).toEqual(["one", "two"]);
+  });
+});
