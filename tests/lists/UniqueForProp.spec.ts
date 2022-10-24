0cda01e90e8d4b60b71412adc88de951bd8aaa35
Ken Snyder <ken@ken.net>
2022-10-23 21:57:40 -0700

    feature: added dictArr API

--- tests/lists/UniqueForProp.spec.ts
+++ tests/lists/UniqueForProp.spec.ts
@@ -1,24 +1,61 @@
 import { describe, it, expect } from "vitest";
 
-import { Expect, Equal, ExpectExtends } from "@type-challenges/utils";
+import { Expect, Equal } from "@type-challenges/utils";
 import { UniqueForProp } from "src/types/lists";
+import { Narrowable } from "src";
+
+const narrow_data = [
+  { id: 123, color: "blue" },
+  { id: 456, color: "red" },
+] as const;
+type NarrowData = typeof narrow_data;
+
+type R = { id: number; color: string };
+const wide_data: readonly R[] = [
+  { id: 123, color: "blue" },
+  { id: 456, color: "red" },
+];
+type WideData = typeof wide_data;
+
+type R2<T extends string> = Readonly<{ id: number; color: T }>;
+const hybrid = <H extends R2<any>>(...data: H[]): readonly H[] => data;
+const hybrid_data = hybrid(
+  ...[
+    { id: 123, color: "blue" },
+    { id: 456, color: "red" },
+  ]
+);
+type HybridData = typeof hybrid_data;
 
 describe("UniqueForProp<T, P>", () => {
-  it("pull off uniques for 'id' prop", () => {
-    const data = [
-      { id: 123, color: "blue" },
-      { id: 456, color: "red" },
-    ] as const;
-    type Data = typeof data;
-    type U = UniqueForProp<Data, "id">;
+  it("narrow data works as expected", () => {
+    type U = UniqueForProp<NarrowData, "id">;
 
     type cases = [
       // the expected keys are part of the union
-      Expect<ExpectExtends<U, 123>>,
-      Expect<ExpectExtends<U, 456>>,
-      // but so is a lot other junk
       Expect<Equal<U, 123 | 456>>
     ];
+
+    const c: cases = [true];
+    expect(c).toBe(c);
+  });
+
+  it("wide data only knows the wide type", () => {
+    type U = UniqueForProp<WideData, "id">;
+
+    type cases = [
+      // the keys are rolled up the wide type
+      Expect<Equal<U, number>>
+    ];
+
+    const c: cases = [true];
+    expect(c).toBe(c);
+  });
+
+  it("wide data only knows the wide type", () => {
+    type U = UniqueForProp<HybridData, "id">;
+
+    type cases = [Expect<Equal<U, number>>];
 
     const c: cases = [true, true, true];
     expect(c).toBe(c);
