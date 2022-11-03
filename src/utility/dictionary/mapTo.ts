9c4d2e8a4de8fdc470e170d1043067acc1b380c4
Ken Snyder <ken@ken.net>
2022-11-02 17:03:29 -0700

    chore: all mapTo tests are passing

--- src/utility/dictionary/mapTo.ts
+++ src/utility/dictionary/mapTo.ts
@@ -1,8 +1,7 @@
 import {
-  MapDirection,
+  MapDirectionVal,
   MapFn,
   MapFnInput,
-  MapInput,
   MapperCardinalityIn,
   MapperCardinalityOut,
   MapTo,
@@ -12,7 +11,7 @@ import { createFnWithProps } from "../createFnWithProps";
 
 export interface MapConfig<
   IR extends OptRequired = "req",
-  D extends MapDirection = "I -> O[]",
+  D extends MapDirectionVal = "I -> O[]",
   OR extends OptRequired = "opt"
 > {
   input?: IR;
@@ -31,7 +30,7 @@ export type MapperApiDefault = <I, O>(map: MapTo<I, O>) => MapFn<I, O>;
  */
 export type MapperApiConfigured = <
   IR extends OptRequired = "req",
-  D extends MapDirection = "I -> O[]",
+  D extends MapDirectionVal = "I -> O[]",
   OR extends OptRequired = "opt"
 >(
   config: MapConfig<IR, D, OR>
@@ -43,7 +42,7 @@ export type MapperApiConfigured = <
 const mapper =
   <
     IR extends OptRequired = "req",
-    D extends MapDirection = "I -> O[]",
+    D extends MapDirectionVal = "I -> O[]",
     OR extends OptRequired = "opt"
   >(
     config: MapConfig<IR, D, OR> = {}
@@ -75,6 +74,17 @@ const mapper =
     return fn as unknown as MapFn<I, O, IR, D, OR>;
   };
 
+/**
+ * **mapTo** _utility_
+ *
+ * This utility creates a strongly typed data mapper which maps from one
+ * known source `I` to another `O`:
+ * ```ts
+ * const mapper = mapTo<I, O>(i => [{
+ *    foo: i.bar
+ * }]);
+ * ```
+ */
 export const mapToFn = <I, O>(map: MapTo<I, O>) => {
   return mapper()<I, O>(map);
 };
@@ -84,9 +94,10 @@ export const mapToFn = <I, O>(map: MapTo<I, O>) => {
  * and _outputs_ to be configured.
  */
 export const mapToDict = {
+  /** configure the relationship between the _inputs_ and _outputs_ */
   config: <
     IR extends OptRequired = "req",
-    D extends MapDirection = "I -> O[]",
+    D extends MapDirectionVal = "I -> O[]",
     OR extends OptRequired = "opt"
   >(
     c: MapConfig<IR, D, OR> = { input: "req", output: "opt", direction: "I -> O[]" } as MapConfig<
@@ -95,6 +106,16 @@ export const mapToDict = {
       OR
     >
   ) => ({
+    /**
+     * create your mapping with your recently setup configuration:
+     * ```ts
+     * const mapper = mapTo
+     *  .config({ ... })
+     *  .map<I, O>(i => [{
+     *    foo: i.bar
+     * }]);
+     * ```
+     */
     map<I, O>(map: MapTo<I, O, IR, D, OR>): MapFn<I, O, IR, D, OR> {
       return mapper(c)(map);
     },
@@ -108,71 +129,3 @@ export const mapToDict = {
  * known source `I` to another `O`.
  */
 export const mapTo = createFnWithProps(mapToFn, mapToDict);
-
-/**
- * **mapTo**
- *
- * The **mapTo()** utility provides a convenient way to generate a type safe
- * type mapper between `I` and `O` types. You will need to set one or possibly
- * two variables to be ready:
- *
- * 1. The mapper function
- * 2. Set the _cardinality_ you want for the **input** and **output** of the function
- *    - **input**: the input's default cardinality is `0:1`; meaning that there will be zero or one
- * inputs each time the function is called.
- *    - **output**: the output's default cardinality is `0:M` which means that you are expected to
- * return an _array_ of `O` but you can have an empty array to filter out the input.
- *
- * In the default scenario you do your mapping with the cardinality constraints specified
- * but the exported function will have additional optionality on input to allow to
- * use cases:
- * ```ts
- * // <S extends I || I[]>(source?: S)
- * //    => S extends I[] ? O[] : O | null;
- * const m = mapTo<I,O>(i => [{
- *    foo: i.title, bar: i.name
- * }]);
- * // as a map fn with `null` values for map filters
- * const ex1 = items.map(m);
- * // convert to an array of `O[]`; filtered items removed automatically
- * const ex2 = m(items);
- * ```
- */
-export const mapToOld = <
-  // input to mapper
-  I extends {},
-  // output to mapper
-  O extends {},
-  // cardinality of input
-  CI extends MapperCardinalityIn = "0:1",
-  CO extends MapperCardinalityOut = "0:M"
->(
-  cb: MapTo<I, O, CI, CO>
-) => {
-  const cardinality = {
-    input: "0:1" as CI,
-    output: "0:M" as CO,
-  };
-
-  const mapFn = (source?: I | I[] | undefined) => {
-    const isArray = Array.isArray(source);
-
-    if (isArray) {
-      // array based value
-      return cardinality.input.startsWith("M")
-        ? cardinality.output.endsWith("M")
-          ? // M:M
-            source.flatMap((i) => cb(i)).filter((i) => i !== null)
-          : // M:1/0
-            cb(source)
-        : // 0/1:M
-          source.flatMap((i) => cb(i)).filter((i) => i !== null);
-    } else {
-      // non-array
-      const result = source ? cb(source) : undefined;
-      return result ? result : [];
-    }
-  };
-
-  return mapFn as MapTo<I, O, CI, CO>;
-};
