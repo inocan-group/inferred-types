a0282ccd395dd0e35324d9e5a861197f828ddb8b
Ken Snyder <ken@ken.net>
2022-11-04 11:18:08 -0700

    feature: added IfXXX<T,IF,ELSE> type utilities to complement boolean IsXXX<T> utils

--- src/types/TypeInfo/IsBooleanLiteral.ts
+++ src/types/TypeInfo/IsBooleanLiteral.ts
@@ -5,3 +5,12 @@
  * just the wider _boolean_ type.
  */
 export type IsBooleanLiteral<T extends boolean> = boolean extends T ? false : true;
+
+/**
+ * **IfBooleanLiteral**
+ *
+ * Branch utility which returns `IF` type when `T` is a boolean literal and `ELSE` otherwise
+ */
+export type IfBooleanLiteral<T extends boolean, IF, ELSE> = IsBooleanLiteral<T> extends true
+  ? IF
+  : ELSE;
--- src/types/TypeInfo/IsNumericLiteral.ts
+++ src/types/TypeInfo/IsNumericLiteral.ts
@@ -5,3 +5,12 @@
  * just the _number_ type.
  */
 export type IsNumericLiteral<T extends number> = number extends T ? false : true;
+
+/**
+ * **IfNumericLiteral**
+ *
+ * Branch utility which returns `IF` type when `T` is a numeric literal and `ELSE` otherwise
+ */
+export type IfNumericLiteral<T extends number, IF, ELSE> = IsNumericLiteral<T> extends true
+  ? IF
+  : ELSE;
