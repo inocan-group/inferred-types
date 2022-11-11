4b18541ba45045cb194fd6e13fecda41448bcbea
Ken Snyder <ken@ken.net>
2022-11-11 15:43:30 -0800

    feat: added isNull(val, if, else) utility

--- src/utility/runtime/conditions/isNull.ts
+++ src/utility/runtime/conditions/isNull.ts
@@ -1,3 +1,20 @@
+export type IsNull<T> = T extends null ? true : false;
+
 export function isNull<T>(i: T) {
   return (i === null) as T extends null ? true : false;
+}
+
+/**
+ * **ifNull**
+ *
+ * Strongly type-aware conditional statement which checks whether a value is
+ * Null and returns one of two values (strongly typed) based on the evaluation
+ * of this criteria.
+ *
+ * @param val the value being tested
+ * @param ifVal the value (strongly typed) returned if val is `null`
+ * @param elseVal the value (strongly typed) returned if val is NOT `null`
+ */
+export function ifNull<T, IF, ELSE>(val: T, ifVal: IF, elseVal: ELSE) {
+  return (isNull(val) ? ifVal : elseVal) as IsNull<T> extends true ? IF : ELSE;
 }
