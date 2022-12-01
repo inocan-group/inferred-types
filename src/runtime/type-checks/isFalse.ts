c66dbccbc9647fbcaef296aae7cd22a3c83baf22
Ken Snyder <ken@ken.net>
2022-12-01 15:26:49 +0000

    feature: added ifFalse<T,IF,ELSE>() runtime utility

--- src/runtime/type-checks/isFalse.ts
+++ src/runtime/type-checks/isFalse.ts
@@ -1,17 +1,29 @@
+import { IfFalse, IsFalse } from "src/types/boolean-logic/boolean";
 import { Narrowable } from "src/types/Narrowable";
-import { IsBoolean } from "./isBoolean";
-
-export type IsFalse<T extends Narrowable> = IsBoolean<T> extends true
-  ? // is a boolean
-    T extends false
-    ? true
-    : true extends T
-    ? false
-    : // is of boolean type; therefore narrow type is unknown
-      unknown
-  : // not a boolean
-    false;
 
 export function isFalse<T>(i: T) {
   return (typeof i === "boolean" && !i) as IsFalse<T>;
+}
+
+/**
+ * **ifTrue**
+ *
+ * Strongly type-aware conditional statement which checks whether a value is
+ * a _true_ and returns one of two values (strongly typed) based on the evaluation
+ * of this criteria.
+ *
+ * @param val the value being tested
+ * @param ifVal the value (strongly typed) returned if val is _true_ value
+ * @param elseVal the value (strongly typed) returned if val is NOT a _true_ value
+ *
+ * Note: at runtime there's no way to distinguish if the value was widely or loosely
+ * typed so unlike the type utility there is no "MAYBE" state but if a wide type if
+ * encountered the _type_ will the union of `IF` and `ELSE`.
+ */
+export function ifFalse<T extends Narrowable, IF extends Narrowable, ELSE extends Narrowable>(
+  val: T,
+  ifVal: IF,
+  elseVal: ELSE
+) {
+  return (isFalse(val) ? ifVal : elseVal) as IfFalse<T, IF, ELSE, IF | ELSE>;
 }
