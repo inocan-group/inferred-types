c72b63aeaa4606d34f04b1b2469108a0aa3c208a
Ken Snyder <ken@ken.net>
2022-12-05 11:12:59 -0800

    feat: StripStarting<T,U> type utility added

---
+++ src/types/alphabetic/StripStarting.ts
@@ -0,0 +1,23 @@
+import { IfLiteral } from "../boolean-logic/IsLiteral";
+
+/**
+ * **StripStarting**`<T, U>`
+ *
+ * Will strip off of `T` the starting string defined by `U` when
+ * both are string literals.
+ * ```ts
+ * type T = "Hello World";
+ * type U = "Hello ";
+ * // "World"
+ * type R = StripStarting<T,U>;
+ * ```
+ */
+export type StripStarting<T extends string, U extends string> = IfLiteral<
+  // can only operate on literal strings
+  T,
+  // this path represents successful strip opp
+  // but we must never accept `U` being wide
+  string extends U ? never : T extends `${U}${infer After}` ? After : T,
+  // here we must stay wide
+  string
+>;
