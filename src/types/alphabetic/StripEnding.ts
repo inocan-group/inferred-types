7854a9e268f0f197deef65b0f543d46e37bd5f63
Ken Snyder <ken@ken.net>
2022-12-05 11:11:43 -0800

    feat: StripEnding<T,U> type utility added

---
+++ src/types/alphabetic/StripEnding.ts
@@ -0,0 +1,23 @@
+import { IfLiteral } from "../boolean-logic/IsLiteral";
+
+/**
+ * **StripEnding**`<T, U>`
+ *
+ * Will strip off of `T` the ending defined by `U` when
+ * both are string literals.
+ * ```ts
+ * type T = "Hello World";
+ * type U = " World";
+ * // "Hello"
+ * type R = StripEnding<T,U>;
+ * ```
+ */
+export type StripEnding<T extends string, U extends string> = IfLiteral<
+  // can only operate on literal strings
+  T,
+  // this path represents successful strip opp
+  // but we must never accept `U` being wide
+  string extends U ? never : T extends `${infer Before}${U}` ? Before : T,
+  // here we must stay wide
+  string
+>;
