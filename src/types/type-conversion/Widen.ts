386bd3867ad8c0016cad7153b4a5be3060e1e708
Ken Snyder <ken@ken.net>
2022-12-08 11:21:34 -0800

    fix: make Widen<T> better at widening Tuple types

--- src/types/type-conversion/Widen.ts
+++ src/types/type-conversion/Widen.ts
@@ -1,9 +1,21 @@
+import { AnyFunction } from "src/runtime";
+
 export type Widen<T> = T extends string
   ? string
   : T extends number
   ? number
   : T extends boolean
   ? boolean
+  : T extends readonly string[]
+  ? string[]
+  : T extends readonly number[]
+  ? number[]
+  : T extends readonly boolean[]
+  ? boolean[]
+  : T extends readonly AnyFunction[]
+  ? AnyFunction[]
+  : T extends readonly any[]
+  ? unknown[]
   : T extends {}
   ? {}
   : T;
