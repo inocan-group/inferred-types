62948b61b0cdba1c8972fd028a29647ccda293ff
Ken Snyder <ken@ken.net>
2022-12-10 11:13:28 -0800

    feat: added IfEqual<X,Y,IF,ELSE> type util feat: added IfNotEqual<X,Y,IF,ELSE> type util

--- src/types/boolean-logic/equivalency.ts
+++ src/types/boolean-logic/equivalency.ts
@@ -1,5 +1,7 @@
+import { Narrowable } from "../Narrowable";
+
 /**
- * **Equal**`<X,Y>`
+ * **Equal**`<X extends Narrowable,Y>`
  *
  * Type utility which tests whether two types -- `X` and `Y` -- are exactly the same type
  */
@@ -12,4 +14,37 @@ export type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T ex
  *
  * Type utility which tests whether two types -- `X` and `Y` -- are _not_ exactly the same type
  */
-export type NotEqual<X, Y> = true extends IsEqual<X, Y> ? false : true;
+export type IsNotEqual<X, Y> = true extends IsEqual<X, Y> ? false : true;
+
+
+
+/**
+ * **IfEqual**`<X,Y,IF,ELSE>`
+ * 
+ * Type utility which returns type `IF` _if_ `X` is equivalent to `Y`; otherwise returns
+ * type `ELSE`.
+ */
+export type IfEqual<
+  X extends Narrowable,
+  Y extends Narrowable, 
+  IF extends Narrowable, 
+  ELSE extends Narrowable
+> = IsEqual<X,Y> extends true
+  ? IF
+  : ELSE;
+
+
+/**
+ * **IfEqual**`<X,Y,IF,ELSE>`
+ * 
+ * Type utility which returns type `IF` _if_ `X` is equivalent to `Y`; otherwise returns
+ * type `ELSE`.
+ */
+export type IfNotEqual<
+  X extends Narrowable,
+  Y extends Narrowable, 
+  IF extends Narrowable, 
+  ELSE extends Narrowable
+> = IsNotEqual<X,Y> extends true
+  ? IF
+  : ELSE;
