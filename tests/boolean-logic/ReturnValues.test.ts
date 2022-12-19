7a8a5c3651e437efbeb46f46e643da37102b80b9
Ken Snyder <ken@ken.net>
2022-12-18 20:01:30 -0800

    feat: added ReturnValues<T> type utility

---
+++ tests/boolean-logic/ReturnValues.test.ts
@@ -0,0 +1,26 @@
+import { Equal, Expect } from "@type-challenges/utils";
+import { ReturnValues } from "src/types/boolean-logic";
+import { describe, it } from "vitest";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to 
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("ReturnValues<T>", () => {
+
+  it("happy path", () => {
+    type T1 = ReturnValues<[]>;
+    type T2 = ReturnValues<["foo","bar","baz"]>;
+    type T3 = ReturnValues<["foo", () => false, () => true]>;
+    type T4 = ReturnValues<["foo", () => false, () => boolean]>;
+    
+    type cases = [
+      Expect<Equal<T1, []>>, //
+      Expect<Equal<T2, []>>,
+      Expect <Equal<T3, [false, true]>>,
+      Expect <Equal<T4, [false, boolean]>>,
+    ];
+    const cases: cases = [ true, true, true, true];
+  });
+
+});
