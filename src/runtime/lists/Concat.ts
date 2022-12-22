4d064d1d54965f9686b1ab1361a14570d12ecda2
Ken Snyder <ken@ken.net>
2022-12-21 17:56:48 -0800

    chore: additional tests and small fix for Concat<T>

--- src/runtime/lists/Concat.ts
+++ src/runtime/lists/Concat.ts
@@ -1,14 +1,37 @@
+import { IfLiteral } from "src/types/boolean-logic";
 import { AfterFirst } from "src/types/lists/AfterFirst";
 import { First } from "src/types/lists/First";
 
-type ConcatElements = readonly (string | number | boolean)[] | ((string | number | boolean)[]);
+
+
+type ConcatElement =  (string | number | boolean);
+type ElementLiteral<T extends ConcatElement> = T extends string 
+  ? IfLiteral<T, T, string>
+  : T extends number
+    ? IfLiteral<T, T, `${number}`>
+    : T extends boolean
+      ? IfLiteral<T, T, `${boolean}`>
+      : never;
 
 type ConcatAcc<
-  T extends readonly ConcatElements, 
+  T extends readonly ConcatElement[] | ConcatElement[], 
   Result extends string = ""
 > = [] extends T
     ? Result
-    : ConcatAcc<AfterFirst<T>, `${Result}${First<T>}`>;
+    : ConcatAcc<AfterFirst<T>, `${Result}${ElementLiteral<First<T>>}`>;
 
-export type Concat<T extends ConcatElements> = ConcatAcc<T>;
+/**
+ * **Concat**`<T>`
+ * 
+ * A type utility which converts an array of strings into a
+ * _concatenated_ string type.
+ * 
+ * ```ts
+ * // `${string}-${string}`
+ * type T1 = Concat<string, "-", string>;
+ * // `foo-bar-baz`
+ * type T2 = Concat<"foo", "-", "bar", "-", "baz">;
+ * ```
+ */
+export type Concat<T extends readonly ConcatElement[] | ConcatElement[], > = ConcatAcc<T>;
 
--- src/types/lists/Concat.test.ts
+++ src/types/lists/Concat.test.ts
@@ -12,13 +12,15 @@ describe("Concat<T>", () => {
     type T1 = Concat<["foo"]>; // foo
     type T2 = Concat<["foo", "bar", "baz"]>; // foobarbaz
     type T3 = Concat<["foo", 42, "bar"]>; //foo42bar
+    type T4 = Concat<["foo", "-is-", true]>;
     
     type cases = [
       Expect<Equal<T1, "foo">>, //
       Expect<Equal<T2, "foobarbaz">>, 
       Expect<Equal<T3, "foo42bar">>, 
+      Expect<Equal<T4, "foo-is-true">>, 
     ];
-    const cases: cases = [true, true, true];
+    const cases: cases = [true, true, true, true];
   });
 
   
@@ -26,14 +28,16 @@ describe("Concat<T>", () => {
     type T1 = Concat<[string]>; 
     type T2 = Concat<[string, string, string]>; 
     type T3 = Concat<[string, number, string]>; 
+    type T4 = Concat<[string, number, boolean]>; 
     
     type cases = [
       Expect<Equal<T1, string>>, //
       Expect<Equal<T2, string>>, 
       Expect<Equal<T3, `${string}${number}${string}`>>, 
+      Expect<Equal<T4, `${string}${number}${boolean}`>>, 
     ];
 
-    const cases: cases = [true, true, true];
+    const cases: cases = [true, true, true, true];
     
   });
 
