abd46f2ba2fb999a813274d7002d43f7d16dac1b
Ken Snyder <ken@ken.net>
2022-12-23 12:09:10 -0800

    fix: Mutable<T> improved to hit all relevant use cases fix: MutableProps<T>  accepts both union types as well as readonly arrays; also made behavior consistent across all use cases feat: added MutablePropsExclusive<T> which is a derivative of MutableProps<T>

--- src/types/dictionary/MutableProps.ts
+++ src/types/dictionary/MutableProps.ts
@@ -1,19 +1,79 @@
 import { ExpandRecursively } from "../ExpandRecursively";
 import { Keys } from "../Keys";
-import { Mutable } from "../Mutable";
+import { AfterFirst } from "../lists";
+import { First } from "../lists/First";
+
+import {  TupleToUnion, UnionToTuple } from "../type-conversion";
+import { Mutable } from "../type-conversion/Mutable";
+import {  WithKeys, WithoutKeys } from "./WithKeys";
+
+type MutablePropAcc<
+  T extends {},
+  MutableKeys extends readonly (keyof T & string)[],
+  Processed extends Readonly<{}> = Readonly<{}>
+> = [] extends MutableKeys
+  ?  ExpandRecursively<Processed>
+  : MutablePropAcc<
+      T, 
+      AfterFirst<MutableKeys>, 
+      Processed & Mutable<Pick<T, First<MutableKeys>>>
+    >;
 
 /**
+ * **MutableProps**`<T,M>`
+ * 
  * Given a dictionary of type `<T>`, this utility function will
- * make the `<M>` generic property _mutable_ and all other _read-only_.
+ * make the properties represented by `M` _mutable_. `M` can be
+ * a union type (as shown below) or a union.
  *
  * ```ts
- * // { foo: string, bar?: Readonly<number> }
+ * // { foo: string, bar?: number, readonly baz: number }
  * type Example = MutableProps<{
- *    foo: Readonly<string>,
- *    bar?: number
+ *    readonly foo: string,
+ *    bar?: number,
+ *    readonly baz: number
+ * }, "foo" | "bar">;
+ * ```
+ * 
+ * **Related:** `MutablePropsExclusive`
+ */
+export type MutableProps<
+  T extends {}, 
+  M extends readonly (keyof T & string)[] | (keyof T & string)
+> = ExpandRecursively<
+WithoutKeys<T, M> & MutablePropAcc<
+  T, 
+  M extends readonly (keyof T & string)[]
+    ? M  
+    : M extends (keyof T & string) 
+      ? UnionToTuple<M> extends readonly (keyof T & string)[]
+        ? UnionToTuple<M>
+        : never
+      : never
+  >
+>;
+
+/**
+ * **MutablePropsExclusive**`<T,M>`
+ * 
+ * Given a dictionary of type `<T>`, this utility function will
+ * make the properties represented by `M` _mutable_ and all others
+ * immutable.
+ *
+ * ```ts
+ * // { foo: string, readonly bar: number, readonly baz: number }
+ * type Example = MutableProps<{
+ *    readonly foo: string,
+ *    bar?: number,
+ *    readonly baz: number
  * }, "foo">;
  * ```
+ * 
+ * **Related:** `MutableProps`
  */
-export type MutableProps<T extends {}, M extends keyof T & string> = ExpandRecursively<
-  Mutable<Pick<T, M>> & Readonly<Pick<T, Keys<T, M>>>
+export type MutablePropsExclusive<
+  T extends {}, 
+  M extends (keyof T & string) | readonly (keyof T & string)[]
+> = ExpandRecursively<
+  Mutable<WithKeys<T,M>> & Readonly<WithoutKeys<T, M>>
 >;
--- tests/dictionary/MutableProps.test.ts
+++ tests/dictionary/MutableProps.test.ts
@@ -1,30 +1,88 @@
 import { describe, it, expect } from "vitest";
 import type { Expect, Equal } from "@type-challenges/utils";
 
-import { MutableProps } from "src/types/dictionary";
+import { MutableProps, MutablePropsExclusive } from "src/types/dictionary";
+import { WithKeys, WithoutKeys } from "src/types/dictionary/WithKeys";
+import { Mutable } from "src/types/type-conversion/Mutable";
 
-describe("MutableProps<T,M>", () => {
-  it("works as expected", () => {
-    type Test = { foo: string; bar?: number; baz: Readonly<boolean> };
-    type Foo = MutableProps<Test, "foo" | "bar">;
+describe("MutableProp<T,M> and MutableProps<T,M>", () => {
+  it("MutableProp<T,M> ", () => {
+    type Test = { foo: string; bar?: number; readonly baz: boolean };
+    type Foo = MutableProps<Test, ["foo", "bar"]>;
+    type FooAlt = MutableProps<Test, "foo"| "bar">;
     type Bar = MutableProps<Test, "bar">;
+    type Bar2 = MutableProps<Test, ["bar"]>;
     type FooBar = MutableProps<Test, "foo" | "bar">;
     type FooBaz = MutableProps<Test, "foo" | "baz">;
 
     type cases = [
-      Expect<Equal<Foo, { foo: string; bar?: Readonly<number>; readonly baz: Readonly<boolean> }>>,
-      Expect<
-        Equal<
+      Expect<Equal<
+        Foo, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+        FooAlt, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
           Bar,
-          { readonly foo: Readonly<string>; bar?: number; readonly baz: Readonly<boolean> }
-        >
-      >,
-      Expect<Equal<FooBar, { foo: string; bar?: number; readonly baz: Readonly<boolean> }>>,
-      Expect<
-        Equal<FooBaz, { foo: string; readonly bar?: Readonly<number>; baz: Readonly<boolean> }>
-      >
+          { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+          Bar2,
+          { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+        FooBar, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+          FooBaz, 
+          { foo: string; bar?: number; baz: boolean }
+      >>
     ];
-    const cases: cases = [true, true, true, true];
+    const cases: cases = [true, true, true, true, true, true];
     expect(cases).toBe(cases);
   });
+
+  
+  it("MutablePropsExclusive<T,M>", () => {
+    type Test = { foo: string; bar?: number; readonly baz: boolean };
+    type Foo = MutablePropsExclusive<Test, ["foo", "bar"]>;
+    type Foo2 = MutablePropsExclusive<Test, "foo" | "bar">;
+    type Bar = MutablePropsExclusive<Test, ["bar"]>;
+    type Bar2 = MutablePropsExclusive<Test, "bar">;
+    type FooBar = MutablePropsExclusive<Test, "foo" | "bar">;
+    type FooBaz = MutablePropsExclusive<Test, "foo" | "baz">;
+
+    type cases = [
+      Expect<Equal<
+        Foo, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+        Foo2, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+          Bar,
+          { readonly foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+          Bar2,
+          { readonly foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+        FooBar, 
+        { foo: string; bar?: number; readonly baz: boolean }
+      >>,
+      Expect<Equal<
+          FooBaz, 
+          { foo: string; readonly bar?: number; baz: boolean }
+      >>
+    ];
+    const cases: cases = [true, true, true, true, true, true];
+    expect(cases).toBe(cases);
+  });
+  
 });
