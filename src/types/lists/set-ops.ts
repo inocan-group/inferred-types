dddd816a274ccd0354fd54c650bf80722ac87dac
Ken Snyder <ken@ken.net>
2022-12-22 16:32:14 -0800

    feat: added Set<T> and SetRemoval<TSet, TRemoval> type utils

---
+++ src/types/lists/set-ops.ts
@@ -0,0 +1,48 @@
+import { IfReadonlyArray, IfSomeEqual } from "../boolean-logic";
+import { Narrowable } from "../Narrowable";
+import { UnionToTuple } from "../type-conversion/UnionToTuple";
+import { AfterFirst } from "./AfterFirst";
+import { First } from "./First";
+
+type SetRemovalAcc<
+  TSet extends readonly any[],
+  TRemoval extends readonly any[],
+  Processed extends readonly any[] = [],
+> = [] extends TSet
+  ? Processed
+  : IfSomeEqual<
+      First<TSet>, TRemoval,
+      SetRemovalAcc<AfterFirst<TSet>, TRemoval, Processed>,
+      SetRemovalAcc<AfterFirst<TSet>, TRemoval, readonly [...Processed, First<TSet>]>
+    >;
+
+/**
+ * **Set**`<T>`
+ * 
+ * Receives either a readonly array of items or union type which 
+ * will be converted into the readonly array.
+ * 
+ * ```ts
+ * // readonly ["foo", "bar", "baz"]
+ * type T1 = Set<"foo" | "bar" | "baz">;
+ * type T2 = Set<["foo", "bar", "baz"]>;
+ * ```
+ */
+export type Set<T extends readonly any[] | Narrowable> = IfReadonlyArray<
+  T, 
+  T extends readonly any[] ? readonly [...T] : never, 
+  readonly [...UnionToTuple<T>]
+>;
+
+/**
+ * **SetRemoval**`<TSet, TRemoval>`
+ * 
+ * Takes two sets -- `TSet` as the primary set, and `TRemoval` -- and produces the
+ * set subtraction of `TSet` minus the elements in `TRemoval`.
+ */
+export type SetRemoval<
+  TSet extends readonly any[] | Narrowable,
+  TRemoval extends readonly any[] | Narrowable,
+> = SetRemovalAcc<Set<TSet>, Set<TRemoval>>;
+
+
