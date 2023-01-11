01b3ed033eff2076f0741c3ff6bd7da82f6646db
Ken Snyder <ken@ken.net>
2023-01-10 18:45:17 -0800

    feat: added case conversions for camelCase, snake_case, PascalCase, and kebab-case

--- src/types/alphabetic/CamelCase.ts
+++ src/types/alphabetic/CamelCase.ts
@@ -1,3 +1,29 @@
+import { IfTrue } from "../boolean-logic/boolean";
+import { LeftWhitespace } from "src/types/string-literals/LeftWhitespace";
+import { RightWhitespace } from "src/types/string-literals/RightWhitespace";
 import { PascalCase } from "./PascalCase";
+import { Concat } from "src/runtime/lists/Concat";
 
-export type CamelCase<S extends string> = string extends S ? string : Uncapitalize<PascalCase<S>>;
+/**
+ * **CamelCase**`<TString,TPreserveWhitespace>`
+ * 
+ * Converts a string to `CamelCase` format while optionally preserving
+ * surrounding whitespace. 
+ */
+export type CamelCase<
+  TString extends string,
+  TPreserveWhitespace extends boolean = false
+> = IfTrue<
+  TPreserveWhitespace,
+  string extends TString
+    ? string
+    : Concat<[
+      LeftWhitespace<TString>,
+      Uncapitalize<PascalCase<TString>>,
+      RightWhitespace<TString>
+    ]>
+  ,
+  string extends TString
+    ? string
+    : Uncapitalize<PascalCase<TString>>
+>;
--- src/types/alphabetic/SnakeCase.ts
+++ src/types/alphabetic/SnakeCase.ts
@@ -1,6 +1,8 @@
 /* eslint @typescript-eslint/no-unused-vars: "off" */
 
-import { Trim } from "../string-literals";
+import { Concat } from "src/runtime/lists/Concat";
+import { IfTrue } from "../boolean-logic/boolean";
+import { LeftWhitespace, RightWhitespace, Trim } from "../string-literals";
 import { DashUppercase } from "./DashUppercase";
 import { LowerAllCaps } from "./LowerAllCaps";
 
@@ -10,18 +12,41 @@ type SpaceToDash<T extends string> = T extends `${infer Begin}${" "}${infer Rest
   : T;
 
 /**
- * Converts a string literal type to _snake_case_.
+ * **SnakeCase**`<TString,TPreserve>`
+ * 
+ * Converts a string literal type to _snake_case_ and optionally preserves
+ * surrounding whitespace.
  * ```ts
  * // "foo_bar"
  * type T = SnakeCase<"fooBar">;
- * type T = SnakeCase<"FooBar">;
- * type T = SnakeCase<"foo-bar">;
  * type T = SnakeCase<"\n foo bar \t">;
- * ``` */
-export type SnakeCase<S extends string> = string extends S
-  ? string
-  : DashUppercase<
-      Uncapitalize<SpaceToDash<Trim<LowerAllCaps<S>>>>
+ * ``` 
+ */
+export type SnakeCase<
+  S extends string,
+  TPreserve extends boolean = false
+> = IfTrue<
+  TPreserve,
+  string extends S
+    ? string
+    : DashUppercase<
+        Uncapitalize<SpaceToDash<Trim<LowerAllCaps<S>>>>
     > extends `${infer Begin}${"-"}${infer Rest}`
-  ? SnakeCase<`${Lowercase<Begin>}_${Rest}`>
-  : Lowercase<DashUppercase<Uncapitalize<Trim<LowerAllCaps<S>>>>>;
+      ? Concat<[
+          LeftWhitespace<S>,
+          SnakeCase<`${Lowercase<Begin>}_${Rest}`>,
+          RightWhitespace<S>
+      ]>
+      : Concat<[
+        LeftWhitespace<S>,
+        Lowercase<DashUppercase<Uncapitalize<LowerAllCaps<S>>>>,
+        RightWhitespace<S>,
+      ]>,
+  string extends S
+    ? string
+    : DashUppercase<
+        Uncapitalize<SpaceToDash<Trim<LowerAllCaps<S>>>>
+      > extends `${infer Begin}${"-"}${infer Rest}`
+    ? SnakeCase<`${Lowercase<Begin>}_${Rest}`>
+    : Lowercase<DashUppercase<Uncapitalize<LowerAllCaps<S>>>>
+>;
