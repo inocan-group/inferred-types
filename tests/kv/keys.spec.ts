f7fb504ba42aa28922e0b81a66581116adc945db
Ken Snyder <ken@ken.net>
2023-01-13 13:29:01 -0800

    refactor: keys() and Keys<T> simplified to do what they say on the tin

--- tests/kv/keys.spec.ts
+++ tests/kv/keys.spec.ts
@@ -4,37 +4,22 @@ import { Keys } from "src/types/Keys";
 import type { Expect, Equal } from "@type-challenges/utils";
 import { defineType, keys } from "src/runtime";
 import { TupleToUnion } from "src/types/type-conversion/TupleToUnion";
+import { UnknownObject } from "src/types";
 
 describe("Keys<T>", () => {
-  it("Keys<T> is shorthand for keyof T", () => {
-    const obj = { foo: 1, bar: 3 };
-    type O = keyof typeof obj;
-    type keys = Keys<typeof obj>;
+  it("happy path", () => {
+    type None = Keys<{}>;
+    type Foobar = Keys<{ foo: 1; bar: 2 }>;
+    type RoFoobar = Keys<Readonly<{ foo: 1; bar: 2 }>>;
 
-    type cases = [Expect<Equal<O, keys>>];
-    const cases: cases = [true];
-    expect(cases).toBe(cases);
-  });
-
-  it("Keys<T> can also convert an array of strings into a union type", () => {
-    const arr = ["foo", "bar"] as const;
-    type Arr = typeof arr;
-    type MyKeys = Keys<Arr>;
-
-    type cases = [Expect<Equal<MyKeys, "foo" | "bar">>];
-    const cases: cases = [true];
-    expect(cases).toBe(cases);
-  });
-
-  it("Keys<T,W> can exclude certain keys", () => {
-    const obj = { foo: 1, bar: 3 };
-    const arr = ["foo", "bar"] as const;
-    type OKeys = Keys<typeof obj, "foo">;
-    type OArr = Keys<typeof arr, "foo">;
-
-    type cases = [Expect<Equal<OKeys, "bar">>, Expect<Equal<OArr, "bar">>];
-    const cases: cases = [true, true];
-    expect(cases).toBe(cases);
+    type cases = [
+      //
+      Expect<Equal<None, []>>,
+      Expect<Equal<Foobar, readonly ["foo", "bar"] & string[]>>,
+      Expect<Equal<RoFoobar, readonly ["foo", "bar"] & string[]>>,
+    ];
+    
+    const cases: cases = [ true, true, true ]; 
   });
 });
 
@@ -55,37 +40,22 @@ describe("keys() utility", () => {
     expect(cases).toBe(cases);
   });
 
-  it("with object and exclusion keys, type and run-time are correct", () => {
-    const obj = { id: "123", color: "red" as string, isFavorite: false as boolean } as const;
-    const k = keys(obj, "color", "id");
-
-    expect(k).toHaveLength(1);
-    expect(k).toContain("isFavorite");
-    expect(k).not.toContain("id");
-    expect(k).not.toContain("color");
-
-    type cases = [
-      Expect<Equal<typeof k, readonly ["isFavorite"]>> //
-    ];
-    const cases: cases = [true];
-    expect(cases).toBe(cases);
-  });
-
   it("empty object results in [] type", () => {
     const t1 = keys({});
-    const t2 = keys([]);
-    const t3 = keys({} as Record<string, any>);
+    const t2 = keys({} as Record<string, any>);
+    const t3 = keys({} as UnknownObject);
+    const t4 = keys({} as Record<string, string>);
 
     expect(t1).toEqual([]);
     expect(t2).toEqual([]);
-    expect(t3).toEqual([]);
 
     type cases = [
-      Expect<Equal<typeof t1, readonly []>>,
-      Expect<Equal<typeof t2, readonly []>>,
-      Expect<Equal<typeof t3, readonly []>>,
+      Expect<Equal<typeof t1, []>>,
+      Expect<Equal<typeof t2, []>>,
+      Expect<Equal<typeof t3, []>>,
+      Expect<Equal<typeof t4, []>>,
     ];
-    const cases: cases = [true, true, true];
+    const cases: cases = [true, true, true, true];
 
   });
 });
