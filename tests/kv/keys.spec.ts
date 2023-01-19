c90e3eb512c161c29e823ae46594c5a0867c4bed
Ken Snyder <ken@ken.net>
2023-01-19 11:43:29 -0800

    feat: added NumericKeys<T> type util feat: improved Keys<T> to resolve numeric keys alone with other subtle improvements

--- tests/kv/keys.spec.ts
+++ tests/kv/keys.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 
-import { Keys } from "src/types/Keys";
+import { Keys, NumericKeys } from "src/types/Keys";
 import type { Expect, Equal } from "@type-challenges/utils";
 import { defineType, keys } from "src/runtime";
 import { TupleToUnion } from "src/types/type-conversion/TupleToUnion";
@@ -8,24 +8,59 @@ import { ExpectExtends, UnknownObject } from "src/types";
 import { ref } from "vue";
 import { isRef } from "src/runtime/type-guards/isRef";
 
-describe("Keys<T>", () => {
+describe("NumericKeys<T>", () => {
+
   it("happy path", () => {
-    type None = Keys<{}>;
-    type Foobar = Keys<{ foo: 1; bar: 2 }>;
-    type RoFoobar = Keys<Readonly<{ foo: 1; bar: 2 }>>;
+    type StringArr = ["foo", "bar", "baz"];
+    type StrArr_RO = readonly ["foo", "bar", "baz"];
+    type NumericArr = [1,2,3]
+    type Numeric = NumericKeys<NumericArr>;
+    type Str = NumericKeys<StringArr>;
+    type Str_RO = NumericKeys<StrArr_RO>;
+    type Empty = NumericKeys<[]>;
 
     type cases = [
       //
+      Expect<Equal<Numeric, readonly ["0", "1", "2"] & (keyof NumericArr)[]>>,
+      Expect<Equal<Str, readonly ["0", "1", "2"] & (keyof StringArr)[]>>,
+      Expect<Equal<Str_RO, readonly ["0", "1", "2"] & (keyof StrArr_RO)[]>>,
+      Expect<Equal<Empty, readonly [] & (keyof [])[]>>,
+    ];
+    
+    const cases: cases = [ true, true, true, true ];
+  });
+});
+
+
+describe("Keys<T>", () => {
+  it("happy path", () => {
+    type Empty = {};
+    type Obj = { foo: 1, bar: 2 };
+    type Obj_RO = Readonly<Obj>;
+
+    type None = Keys<Empty>;
+    type Foobar = Keys<Obj>;
+    type RoFoobar = Keys<Obj_RO>;
+
+    type cases = [
+      // any object with no keys resolves simply to an empty array
       Expect<Equal<None, readonly []>>,
-      Expect<Equal<Foobar, readonly ["foo", "bar"]>>,
-      Expect<Equal<RoFoobar, readonly ["foo", "bar"]>>,
+      // once keys are involved we are not guaranteed ordering of keys
+      Expect<ExpectExtends<
+        (readonly ["foo", "bar"] | readonly ["bar", "foo"]) & (keyof Obj)[], 
+        Foobar
+      >>,
+      Expect<ExpectExtends<
+        (readonly ["foo", "bar"] | readonly ["bar", "foo"]) & (keyof Obj_RO)[], 
+        RoFoobar
+      >>,
     ];
     
     const cases: cases = [ true, true, true ]; 
   });
 });
 
-describe("keys() utility", () => {
+describe("keys() utility on object", () => {
   it("with just object passed in, keys are extracted as expected", () => {
     const obj = defineType({ id: "123" })({ color: "red", isFavorite: false });
     const k = keys(obj); 
@@ -43,15 +78,35 @@ describe("keys() utility", () => {
   });
 
   
+  it("keys() utility on array", () => {
+    const arr = ["foo", "bar", "baz"] as const;
+    const k = keys(arr);
+
+    expect(k).toEqual(["0","1","2"]);
+  });
+  
+
+  
   it("keys of a VueJS Ref<T>", () => {
     const obj = ref({foo: 1, bar: 2});
     const k = keys(obj);
     
+    expect("value" in obj).toBe(true);
+    expect("__v_isRef" in obj).toBe(true);
     expect(isRef(obj)).toBe(true);
+    
     // props at runtime not visible on type
-    expect(k.includes("__v_isRef" as any)).toBe(true);
+    expect(
+      (k as any[]).includes("__v_isRef"),
+      `VueJS ref looks like: ${k}`
+    ).toBe(true);
+
     type cases = [
-      Expect<ExpectExtends<readonly [symbol, "value"], typeof k>>
+      Expect<ExpectExtends<
+        (readonly [symbol, "value"] & (keyof typeof obj)[]) |
+        (readonly ["value", symbol] & (keyof typeof obj)[]), 
+        typeof k
+      >>
     ];
     const cases: cases = [ true ];
 
@@ -59,8 +114,10 @@ describe("keys() utility", () => {
   
 
   it("empty object results in [] type", () => {
-    const t1 = keys({});
-    const t2 = keys({} as Record<string, any>);
+    const empty = {};
+    const anyObject = {} as Record<string, any>;
+    const t1 = keys(empty);
+    const t2 = keys(anyObject);
     const t3 = keys({} as UnknownObject);
     const t4 = keys({} as Record<string, string>);
 
