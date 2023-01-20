d490ce9b71acfd2b33e352d043d660c622913a37
Ken Snyder <ken@ken.net>
2023-01-19 20:15:03 -0800

    fix: fixed EnsureLeading<TTarget,TLeading> type util

---
+++ tests/type-conversions/EnsureLeadingEvery.test.ts
@@ -0,0 +1,21 @@
+import { Equal, Expect } from "@type-challenges/utils";
+import { EnsureLeadingEvery } from "src/types/alphabetic/EnsureLeadingEvery";
+import { describe, it } from "vitest";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to 
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("EnsureLeadingEvery<TList, TLeading>", () => {
+
+  it("happy path", () => {
+    type List = ["foo", 42, "bar"];
+    type T1 = EnsureLeadingEvery<List, "a.">;
+    
+    type cases = [
+      Expect<Equal<T1, readonly ["a.foo", "a.42", "a.bar"]>>,
+    ];
+    const cases: cases = [ true ];
+  });
+
+});
