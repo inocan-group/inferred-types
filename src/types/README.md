8f573870c29da59f815dc7bced8f6f6fa010117b
Ken Snyder <ken@ken.net>
2023-01-27 20:08:34 -0800

    chore: fixed bad imports and small change to TypeGuard type

--- src/types/README.md
+++ src/types/README.md
@@ -1,3 +1,4 @@
 # Types
 
 A directory for types only. These types _will_ be exported as symbols during the build process.
+
--- src/types/TypeGuard.ts
+++ src/types/TypeGuard.ts
@@ -4,4 +4,4 @@
  * a typing for a **TS** type-guard which evaluates an _unknown_ input
  * and determines if it is of type `T`.
  */
-export type TypeGuard<T> = (thing: unknown) => thing is T;
+export type TypeGuard<T> = (value: unknown) => value is T;
