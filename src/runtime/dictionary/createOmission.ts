1fcc2c98ea784579b43c526b64e79cae286e1b48
Ken Snyder <ken@ken.net>
2023-02-20 13:30:22 -0800

    feat: added createOmission() and createRetainer() utils

---
+++ src/runtime/dictionary/createOmission.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Narrowable } from "src/types/literals";
+import { omit } from "./omit";
+
+/**
+ * **createOmission**(...keys)(obj)
+ * 
+ * A higher order function which is _configured_ to exclude a number of
+ * key values. This partial application of the utility can then be reused
+ * as many times as desired to exclude certain 
+ * 
+ * **Related:** `omit`
+ */
+export function createOmission<
+  TKeys extends readonly (string|symbol)[]
+>(...removeKeys: TKeys) {
+  return <
+    TObj extends Record<string|symbol, N>,
+    N extends Narrowable,
+  >(obj: TObj) => omit(obj, ...removeKeys);
+}
---
+++ src/runtime/dictionary/createRetainer.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Narrowable } from "src/types/literals";
+import { retain } from "./retain";
+
+/**
+ * **createRetainer**(...keys)(obj)
+ * 
+ * A higher order function which is _configured_ to "retain" a number of
+ * keys in future objects. This partial application of the utility can then be reused
+ * as many times as desired to retain a subset of these future objects.
+ * 
+ * **Related:** `retain`
+ */
+export function createOmission<
+  TKeys extends readonly (string|symbol)[]
+>(...removeKeys: TKeys) {
+  return <
+    TObj extends Record<string|symbol, N>,
+    N extends Narrowable
+  >(obj: TObj) => retain(obj, ...removeKeys);
+}
