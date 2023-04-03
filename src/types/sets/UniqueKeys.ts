b3d682b6db87979f4b451ed2b2f95b12f62a0f2a
Ken Snyder <ken@ken.net>
2023-04-03 13:32:10 -0700

    fix: UniqueKeys<T> now works as expected for numeric arrays

--- src/types/sets/UniqueKeys.ts
+++ src/types/sets/UniqueKeys.ts
@@ -1,4 +1,4 @@
-import { Container, LeftRight, UnionToTuple } from "src/types";
+import { AsNumberWhenPossible, Container, LeftRight, UnionToTuple } from "src/types";
 
 /**
  * **UniqueKeys**`<L,R>`
@@ -12,13 +12,25 @@ import { Container, LeftRight, UnionToTuple } from "src/types";
  * ```ts
  * type Foo = { foo: 1; bar: 2 };
  * type Baz = { bar: 5; baz: 42, extra: "why not?" };
- * // [ "foo", "baz" | "extra" ]
+ * // LeftRight<["foo"], ["baz", "extra"]>
  * type U = UniqueKeys<Foo,Baz>;
+ * ```
+ * - the example above with an _object_ container is the primary use case
+ * but it can be used with array's too but please note it is primarily used
+ * in this case to identify how the "longer" array has additional keys:
+ * ```ts
+ * type Short = [1,2]; // length 2
+ * type Long = [3,4,5,6,7,8,9,10]; // length 8
+ * // LeftRight<[], [2,3,4,5,6,7]>
+ * type U = UniqueKeys<Short,Long>;
  * ```
  * 
  * **Related:** `UniqueKeysUnion`
  */
-export type UniqueKeys<L extends Container, R extends Container> = LeftRight<
-  UnionToTuple<Exclude<keyof L, keyof R>>,
-  UnionToTuple<Exclude<keyof R, keyof L>>
+export type UniqueKeys<
+  L extends Container, 
+  R extends Container
+> = LeftRight<
+  AsNumberWhenPossible<UnionToTuple<Exclude<keyof L, keyof R>>>,
+  AsNumberWhenPossible<UnionToTuple<Exclude<keyof R, keyof L>>>
 >;
---
+++ tests/dictionary/UniqueKeys.test.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Equal, Expect } from "@type-challenges/utils";
+import { LeftRight, UniqueKeys } from "src/types";
+import { describe, it } from "vitest";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to 
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("UniqueKeys<A,B>", () => {
+
+  it("happy path", () => {
+    type NoOverlap = UniqueKeys<{foo: 1}, {bar: 2; baz: 3}>;
+    type Override = UniqueKeys<{foo: 1}, {foo:42; bar: 2; baz: 3}>;
+    type EmptyLeft = UniqueKeys<{}, { bar: 2; baz: 3}>;
+    type EmptyRight = UniqueKeys<{foo: 1}, {}>;
+
+    type NumShortLong = UniqueKeys<[1,2],[1,2,3,4]>;
+    
+    type cases = [
+      Expect<Equal<NoOverlap, LeftRight<["foo"], ["bar", "baz"]>>>,
+      Expect<Equal<Override, LeftRight<[], ["bar", "baz"]>>>,
+      Expect<Equal<EmptyLeft, LeftRight<[], ["bar", "baz"]>>>,
+      Expect<Equal<EmptyRight, LeftRight<["foo"], []>>>,
+
+      Expect<Equal<NumShortLong, LeftRight<[], [2,3]>>>,
+    ];
+    const cases: cases = [
+      true, true, true, true,
+      true
+    ];
+  });
+
+});
