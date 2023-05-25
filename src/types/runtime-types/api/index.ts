16a3d8d671fc67d024afec8d2a2b20162e70c414
Ken Snyder <ken@ken.net>
2023-05-25 11:35:32 -0700

    chore: type errors which didn't show on iMac

--- src/types/runtime-types/api/index.ts
+++ src/types/runtime-types/api/index.ts
@@ -1,4 +1,4 @@
-import { TypeApiBoolean , TypeApiNumber , TypeApiNumericLiteral , TypeApiString , TypeApiStringLiteral } from "src/types";
+
 
 /**
  * **TypeApi**
@@ -7,20 +7,20 @@ import { TypeApiBoolean , TypeApiNumber , TypeApiNumericLiteral , TypeApiString 
  */
 export type TypeApi = {
   /** a _wide_ string type */
-  string: TypeApiString;
-  /** 
-   * **stringLiteral**(literals,options)
-   * 
-   * A _narrow_ string literal value.
-   * ```ts
-   * // "foo" | "bar" | "baz"
-   * const t1 = type(d => d.stringLiteral(["foo", "bar", "baz"]));
-   * // `Hi ${string}`
-   * const t2 = type(d => d.stringLiteral(s => s.startsWith("Hi ")));
-   * ```
-   */
-  stringLiteral: TypeApiStringLiteral;
-  number: TypeApiNumber;
-  numericLiteral: TypeApiNumericLiteral;
-  boolean: TypeApiBoolean;
+  // string: TypeApiString;
+  // /** 
+  //  * **stringLiteral**(literals,options)
+  //  * 
+  //  * A _narrow_ string literal value.
+  //  * ```ts
+  //  * // "foo" | "bar" | "baz"
+  //  * const t1 = type(d => d.stringLiteral(["foo", "bar", "baz"]));
+  //  * // `Hi ${string}`
+  //  * const t2 = type(d => d.stringLiteral(s => s.startsWith("Hi ")));
+  //  * ```
+  //  */
+  // stringLiteral: TypeApiStringLiteral;
+  // number: TypeApiNumber;
+  // numericLiteral: TypeApiNumericLiteral;
+  // boolean: TypeApiBoolean;
 };
--- src/types/runtime-types/api/numericLiteral.ts
+++ src/types/runtime-types/api/numericLiteral.ts
@@ -1,23 +1,25 @@
 
-import { NoDefaultValue } from "src/constants";
-import { Type, TypeDefaultValue, TypeOptions, TypeUnderlying } from "src/types";
 
-export type TypeApiNumericLiteral = <
-TRequired extends boolean = true,
-TDesc extends string = "",
-TUnderlying extends TypeUnderlying = "no-underlying",
-TDefaultValue extends TypeDefaultValue<"stringLiteral", TRequired, TUnderlying> = NoDefaultValue,
-TValidations extends readonly unknown[] | "no-validations" = "no-validations", 
->(
-  literal: TUnderlying extends readonly string[] 
-    ? TUnderlying 
-    : never, 
-  options?: TypeOptions<"numericLiteral",TRequired, TDesc, TUnderlying, TDefaultValue, TValidations>
-) => Type<
-  "numericLiteral",
-  TRequired extends true ? "required" : "not-required", 
-  TDesc, 
-  TUnderlying, 
-  TDefaultValue extends NoDefaultValue ? "no-default-value" : "with-default-value", 
-  TValidations extends "no-validations" ? "no-validations" : "with-validations"
->;
+
+// import { NoDefaultValue } from "src/constants";
+// import { Type, TypeDefaultValue, TypeOptions, TypeUnderlying } from "src/types";
+
+// export type TypeApiNumericLiteral = <
+// TRequired extends boolean = true,
+// TDesc extends string = "",
+// TUnderlying extends TypeUnderlying = "no-underlying",
+// TDefaultValue extends TypeDefaultValue<"stringLiteral", TRequired, TUnderlying> = NoDefaultValue,
+// TValidations extends readonly unknown[] | "no-validations" = "no-validations", 
+// >(
+//   literal: TUnderlying extends readonly string[] 
+//     ? TUnderlying 
+//     : never, 
+//   options?: TypeOptions<"numericLiteral",TRequired, TDesc, TUnderlying, TDefaultValue, TValidations>
+// ) => Type<
+//   "numericLiteral",
+//   TRequired extends true ? "required" : "not-required", 
+//   TDesc, 
+//   TUnderlying, 
+//   TDefaultValue extends NoDefaultValue ? "no-default-value" : "with-default-value", 
+//   TValidations extends "no-validations" ? "no-validations" : "with-validations"
+// >;
