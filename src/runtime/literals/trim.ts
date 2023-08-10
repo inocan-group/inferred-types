28e5a859868f60a647594af2cd298a8e47f23ce5
Ken Snyder <ken@ken.net>
2023-08-10 01:21:28 -0700

    feat: added surround() and trim() utils

---
+++ src/runtime/literals/trim.ts
@@ -0,0 +1,55 @@
+import { Trim, TrimLeft, TrimRight } from "src/types";
+
+/**
+ * **trim**(input)
+ * 
+ * A runtime utility which trims whitespace on both sides of a string
+ * and returns a type-strong string literal where possible.
+ */
+export function trim<T extends string>(input: T): Trim<T> {
+  return input.trim() as Trim<T>;
+}
+
+/**
+ * **trimLeft**(input)
+ * 
+ * A runtime utility which trims whitespace on the leading/left side of a string
+ * and returns a type-strong string literal where possible.
+ * 
+ * @deprecated prefer use of `trimStart` instead
+ */
+export function trimLeft<T extends string>(input: T): TrimLeft<T> {
+  return input.trimStart() as TrimLeft<T>;
+}
+
+/**
+ * **trimStart**(input)
+ * 
+ * A runtime utility which trims whitespace on the leading/left side of a string
+ * and returns a type-strong string literal where possible.
+ */
+export function trimStart<T extends string>(input: T): TrimLeft<T> {
+  return input.trimStart() as TrimLeft<T>;
+}
+
+/**
+ * **trimRight**(input)
+ * 
+ * A runtime utility which trims whitespace on the tailing/right side of a string
+ * and returns a type-strong string literal where possible.
+ * 
+ * @deprecated prefer use of `trimEnd` instead
+ */
+export function trimRight<T extends string>(input: T): TrimRight<T> {
+  return input.trimEnd() as TrimRight<T>;
+}
+
+/**
+ * **trimEnd**(input)
+ * 
+ * A runtime utility which trims whitespace on the tailing/right side of a string
+ * and returns a type-strong string literal where possible.
+ */
+export function trimEnd<T extends string>(input: T): TrimRight<T> {
+  return input.trimEnd() as TrimRight<T>;
+}
