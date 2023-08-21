c920ddf9319bafbfe5b11a89832dbeef11a6a54e
Ken Snyder <ken@ken.net>
2023-08-21 15:48:55 -0700

    feat: added ExtendsNone type util

---
+++ src/types/boolean-logic/operators/ExtendsNone.ts
@@ -0,0 +1,16 @@
+import type { Not, ExtendsSome } from "src/types";
+
+
+/**
+  * **ExtendsAll**`<TValue, TList>`
+  * 
+  * Boolean type utility which evaluates whether a singular 
+  * value `TValue` extends **all** of the elements in `TList`.
+  * 
+  * **Related:** `ExtendsSome`, `IfExtendsAll`, `DoesExtend`
+  */
+export type ExtendsNone<
+  TValue,
+  TList extends readonly unknown[],
+> = Not<ExtendsSome<TValue, TList>>;
+
