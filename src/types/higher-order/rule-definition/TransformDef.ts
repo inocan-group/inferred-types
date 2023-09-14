12e0474b30d1a76496da967b81689634507c90ba
Ken Snyder <ken@ken.net>
2023-09-14 11:32:40 -0700

    chore: removed some of older higher-order implementation

--- src/types/higher-order/rule-definition/TransformDef.ts
+++ src/types/higher-order/rule-definition/TransformDef.ts
@@ -1,17 +1,59 @@
 
-import { TransformOp, TransformParams  } from "src/types";
+import { AsArray, ComparisonOp, ErrorCondition, MatchDef, NotEqual, OpHandler, TransformOp, TransformParams, Tuple  } from "src/types";
+
+type Identity = "transform-def";
+type Op = 1;
+type Params = 2;
+type Handler = 3;
+type Conditions = 4;
 
 /**
  * **TransformDef**`<TOp>`
  * 
  * A tuple definition of a future transform operation.
+ * 
+ * - these transforms _can be_ conditional but do not need to be
  */
 export type TransformDef<
   TOp extends TransformOp,
-  TParams extends AsArray<TransformParams<TOp>> = AsArray<TransformParams<TOp>> 
+  TParams extends AsArray<TransformParams<TOp>> = AsArray<TransformParams<TOp>>,
+  TCond extends readonly MatchDef<ComparisonOp>[] = []
 > = [
-  "transform-def",
-  TOp,
-  TParams,
-  OpHandler,
+  identity: Identity,
+  op: TOp,
+  params: TParams,
+  handler: OpHandler,
+  conditions: TCond
 ];
+
+/**
+ * **IsTransformDef**`<T>`
+ * 
+ * Boolean checker on whether the provided `T` is a `TransformDef`
+ */
+export type IsTransformDef<T extends Tuple> = T["length"] extends 5
+  ? T[Op] extends TransformOp
+    ? T[Params] extends TransformParams<T[Op]>
+      ? T[Handler] extends OpHandler
+        ? T[Conditions] extends Tuple
+          ? true
+          : false
+        : false
+      : false
+    : false
+  : false;
+
+/**
+ * **IfTransformDef**`<T>`
+ * 
+ * Branching utility based on whether `T` is a `TransformDef` structure
+ * 
+ * - if no `IF` type provided it will default to proxying `T` through
+ * - if no `ELSE` type is provided it will produce a 
+ * `ErrorCondition<"invalid-transform-def">`
+ */
+export type IfTransformDef<
+  T extends Tuple,
+  IF = T,
+  ELSE = ErrorCondition<"invalid-transform-def">
+> = IsTransformDef<T> extends true ? IF : ELSE;
