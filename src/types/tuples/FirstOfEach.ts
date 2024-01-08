538846deffe05464242422b799ae8ce412aebe0b
Ken Snyder <ken@ken.net>
2024-01-08 15:37:10 -0800

    chore: removed higher order tests

--- src/types/tuples/FirstOfEach.ts
+++ src/types/tuples/FirstOfEach.ts
@@ -1,26 +1,16 @@
 /**
- * **FirstOfEach**<TList>
+ * **FirstOfEach**`<TList>`
  * 
- * Mutates a tuple in one of two ways based on the input tuple:
- * 
- * - **String** - if the array is an array of string literals then it will reduce the literal 
- * to just the first letter (if a wide string is found then it is left "as is")
- * - **Array** - if the elements of the array `TList` are themselves array's then it
- * will reduce the item to just the first element in the array and _undefined_ if the
- * array was empty.
- * 
- * For a two-dimensional array, returns a _union type_ which combines the first element of the interior
- * array.
+ * Expects `TList` to consist of an array of arrays and then this utility will reduce
+ * this to a _union_ of all the first elements in `TList`
  *
  * ```ts
- * type Str = [ "foo", "bar", "baz" ];
- * type Arr = [ ["foo", 1], ["bar", 2] ];
- * // "f" | "b"
- * type S = FirstOfEach<Str>;
  * // "foo" | "bar"
- * type A = FirstOfEach<Arr>;
+ * type A = FirstOfEach<[ ["foo", 1], ["bar", 2] ]>;
  * ```
  */
-export type FirstOfEach<T extends readonly unknown[][]> = T[number][0] extends T[number][number]
+export type FirstOfEach<
+  T extends readonly unknown[][]
+> = T[number][0] extends T[number][number]
   ? T[number][0]
   : never;
