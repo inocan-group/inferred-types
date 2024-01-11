8964956400eb12072a71f1bc756faa23a59171cc
Ken Snyder <ken@ken.net>
2024-01-10 16:30:17 -0800

    feat: improvements to AsNumber<T> util

--- src/types/type-conversion/AsNumber.ts
+++ src/types/type-conversion/AsNumber.ts
@@ -1,14 +1,22 @@
 
+
+type ParseInt<T> = T extends `${infer N extends number}` ? N : never;
 /**
- * **AsNumber**<T>
+ * **AsNumber**`<T>`
  * 
- * Attempts to narrow `T` to a number type where possible.
+ * Returns a _number_ for `T` where `T` extends a _number_ or `${number}` type; otherwise
+ * return _never_. Literal types are preserved.
  * 
+ * ```ts
+ * // 4
+ * type Num = AsNumber<"4">;
+ * ```
  * **Related:** `ToNumber`
  */
 export type AsNumber<T> = T extends number
   ? T & number
   : number extends T
     ? T & number
-    : never;
-
+    : T extends `${number}` 
+      ? ParseInt<T>
+      : never;
---
+++ tests/numeric-literals/AsNumber.test.ts
@@ -0,0 +1,31 @@
+;import { Equal, Expect } from "@type-challenges/utils";
+import { AsNumber, IsNever } from "src/types";
+import { describe, it } from "vitest";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to 
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("AsNumber<T>", () => {
+
+  it("Happy Path", () => {
+    type Int = AsNumber<"3">;
+    type Float = AsNumber<"3.4">;
+    type NegInt = AsNumber<"-3">;
+    type NegFloat = AsNumber<"-3.4">;
+    
+    type cases = [
+      Expect<Equal<Int, 3>>,
+      Expect<Equal<Float, 3.4>>,
+      Expect<Equal<NegInt, -3>>,
+      Expect<Equal<NegFloat, -3.4>>,
+
+      IsNever<AsNumber<"foobar">>
+    ];
+    const cases: cases = [ 
+      true, true, true, true, 
+      true 
+    ];
+  });
+
+});
