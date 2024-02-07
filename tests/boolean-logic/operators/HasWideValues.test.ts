d22f837ac7888e9603cc1be60c83380e1575ff78
Ken Snyder <ken@ken.net>
2024-02-07 12:26:17 -0800

    feat: added HasWideValues<T> utility

---
+++ tests/boolean-logic/operators/HasWideValues.test.ts
@@ -0,0 +1,28 @@
+import {  ExpectFalse, ExpectTrue } from "@type-challenges/utils";
+import { describe, it } from "vitest";
+import { HasWideValues } from "../../../src/inferred-types";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to 
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("HasWideValues<T>", () => {
+
+  it("happy path", () => {
+    type T1 = HasWideValues<["foo", 42, string]>;
+
+    type F1 = HasWideValues<[]>;
+    type F2 = HasWideValues<["foo", "bar"]>;
+    
+    type cases = [
+      ExpectTrue<T1>,
+      ExpectFalse<F1>,
+      ExpectFalse<F2>
+    ];
+    const cases: cases = [
+      true,
+      false, false
+    ];
+  });
+
+});
