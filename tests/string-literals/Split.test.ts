f76a9755c97b526f22fb1648393bd562d8cf8fd5
Ken Snyder <ken@ken.net>
2024-02-09 11:51:31 -0800

    chore: Split<T,U> utility can now split on union type but only using the "omit" policy

--- tests/string-literals/Split.test.ts
+++ tests/string-literals/Split.test.ts
@@ -1,7 +1,7 @@
 import { Equal, Expect } from "@type-challenges/utils";
 import { describe, it } from "vitest";
 
-import { IsUnion, Split, UpperAlphaChar } from "src/types/index";
+import {  Split, UpperAlphaChar } from "src/types/index";
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
 // standpoint so always be sure to run `tsc --noEmit` over your test files to 
@@ -10,19 +10,19 @@ import { IsUnion, Split, UpperAlphaChar } from "src/types/index";
 describe("Split<T,SEP>", () => {
 
   it("happy path", () => {
-    type CommaFooBar = Split<"foo,bar", ",">;
+    type CommaFooBarBaz = Split<"foo,bar,baz", ",">;
     type Empty = Split<"">;
-    type AlmostEmpty = Split<"",",">;
+    type EmptyToo = Split<"",",">;
     type Characters = Split<"hello","">;
     
     type cases = [
-      Expect<Equal<CommaFooBar, ["foo", "bar"]>>,
+      Expect<Equal<CommaFooBarBaz, ["foo", "bar", "baz"]>>,
       Expect<Equal<
         Characters, 
         ["h", "e", "l", "l", "o"]
       >>,
       Expect<Equal<Empty, []>>,
-      Expect<Equal<AlmostEmpty, [""]>>,
+      Expect<Equal<EmptyToo, []>>,
     ];
 
     const cases: cases = [
@@ -45,11 +45,16 @@ describe("Split<T,SEP>", () => {
   });
 
   it("Split<T, SEP> where string and separator are same", () => {
-    const str = "hello world, 42 nice to meet you" as const;
-    type S = Split<typeof str, typeof str>;
+    type Str = "hello";
+    type S1 = Split<Str,Str>;
+    const str = "hello world" as const;
+    type S2 = Split<typeof str, typeof str>;
 
-    type cases = [Expect<Equal<S, ["",""]>>];
-    const cases: cases = [true];
+    type cases = [
+      Expect<Equal<S1, ["",""]>>,
+      Expect<Equal<S2, ["",""]>>
+    ];
+    const cases: cases = [true, true];
   });
 
   it("Split<T, SEP> string is wide type", () => {
@@ -62,15 +67,16 @@ describe("Split<T,SEP>", () => {
   
   it("Split with a union type", () => {
     // TODO: fix split's implementation for union types
-    type _X = IsUnion<UpperAlphaChar>;
     type FooBar = Split<"FooBar", UpperAlphaChar>;
     type FooBarOmit = Split<"FooBar", UpperAlphaChar, "omit">;
-    type FooBarRetain = Split<"FooBar", UpperAlphaChar, "retain">;
+    type FooBarLeading = Split<"Foo, Bar; Baz", ", " | "; ", "leading">;
+    type FooBarTrailing = Split<"Foo, Bar, Baz", ", ", "trailing">;
     
     type cases = [
       Expect<Equal<FooBar, ["oo", "ar"]>>,
       Expect<Equal<FooBarOmit, ["oo", "ar"]>>,
-      Expect<Equal<FooBarRetain, ["Foo", "Bar"]>>,
+      Expect<Equal<FooBarLeading, ["Foo", ", Bar", ", Baz"]>>,
+      Expect<Equal<FooBarTrailing, ["Foo, ", "Bar, ", "Baz"]>>,
     ];
     const cases: cases = [ true, true, true ];
     
@@ -78,4 +84,3 @@ describe("Split<T,SEP>", () => {
   
 
 });
-
