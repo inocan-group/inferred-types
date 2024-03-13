19516c7af05ae3f29a28ab026ff095fdd487b2e9
Ken Snyder <ken@ken.net>
2024-03-13 06:26:45 -0700

    feat: added HasProp<TTest,TProp> as boolean operator which can be extended to be a branching util
    feat: added TakeProp<TTest, TProp, TElse>
    
    feat: added AsDefined<T> utility to narrow a type by removing _undefined_ as a possible union type

---
+++ src/types/dictionary/TakeProp.ts
@@ -0,0 +1,16 @@
+
+/**
+ * **TakeProp**`<TTest,TProp,[TElse]>`
+ * 
+ * Takes the property `TTest` and determines if `TProp` is a key of
+ * of `TTest`. If it is it will return `TTest[TProp]` otherwise `TElse`
+ * (which is defaulted to _undefined_).
+ */
+export type TakeProp<
+  TTest,
+  TProp extends PropertyKey,
+  TElse = undefined
+> = TProp extends keyof TTest
+? TTest[TProp]
+: TElse;
+  
