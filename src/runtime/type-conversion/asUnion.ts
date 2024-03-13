b571b1d29a574d1fd544fe256e17c53f77f9e32d
Ken Snyder <ken@ken.net>
2024-03-13 16:39:25 -0700

    feat: added asUnion() runtime util feat: added RuntimeUnion type util

---
+++ src/runtime/type-conversion/asUnion.ts
@@ -0,0 +1,20 @@
+import { UnionToTuple, RuntimeUnion, Narrowable } from "src/types/index"
+
+/**
+ * **asUnion**(...members)
+ * 
+ * Creates a `RuntimeUnion` which acts as a function which returns
+ * the union type but also exposes a `members` property which allows
+ * the runtime to check all elements of the union. 
+ */
+export const asUnion = <
+  T extends readonly Narrowable[]
+>(...members: T) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const fn: any = () => "union" as unknown as UnionToTuple<T>;
+  fn.kind = "Union";
+  fn.members = members;
+  fn.includes = (v: unknown) => members.includes(v);
+
+  return fn as RuntimeUnion<T>;
+}
