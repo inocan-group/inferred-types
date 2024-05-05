31164104d82d17b0622b7fba2fde9ed9c8d7db3b
Ken Snyder <ken@ken.net>
2024-05-05 09:20:43 -0700

    fix: improved `DropChars` to use either a single string pattern (existing) or a single character union

--- tests/string-literals/DropChars.test.ts
+++ tests/string-literals/DropChars.test.ts
@@ -1,15 +1,14 @@
 import { Equal, Expect } from "@type-challenges/utils";
 import { describe, it } from "vitest";
 
-import { DropChars } from "src/types/index";
+import { DropChars, UpperAlphaChar } from "src/types/index";
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
 // standpoint so always be sure to run `tsc --noEmit` over your test files to 
 // gain validation that no new type vulnerabilities have cropped up.
 
 describe("DropChars<TContent,TDrop>", () => {
-
-  it("happy path", () => {
+  it("dropping using a single string sequence", () => {
     type Foobar = DropChars<"foobar", "fb">;
     type Foobarbaz = DropChars<"foo, bar, baz", " b">;
     type All = DropChars<"foo", "fo">;
@@ -22,4 +21,15 @@ describe("DropChars<TContent,TDrop>", () => {
     const cases: cases = [ true, true, true  ];
   });
 
+  
+  it("dropping with a union type", () => {
+    type FooBarBaz = DropChars<"foo, bar, baz", "b" | "f">;
+    type FooBarBaz2 = DropChars<"Foo, Bar, Baz", UpperAlphaChar>;
+    
+    type cases = [
+      Expect<Equal<FooBarBaz, "oo, ar, az">>,
+      Expect<Equal<FooBarBaz2, "oo, ar, az">>,
+    ];
+    const cases: cases = [ true, true ];
+  });
 });
