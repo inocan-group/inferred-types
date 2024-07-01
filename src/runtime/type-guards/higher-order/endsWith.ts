0f16a3bdca2279dc2dd7a730a438e441c8f772fa
Ken Snyder <ken@ken.net>
2024-07-01 14:21:15 -0700

    fix: fixed endsWith()

--- src/runtime/type-guards/higher-order/endsWith.ts
+++ src/runtime/type-guards/higher-order/endsWith.ts
@@ -1,6 +1,5 @@
 import { Narrowable } from "src/types/index";
-import { ifString } from "../../boolean-logic/ifString";
-import { ifNumber } from "../../boolean-logic/ifNumber";
+import { isString, isNumber } from "src/runtime/index";
 
 
 export type EndingWithTypeGuard<T extends string> = <
@@ -9,20 +8,16 @@ export type EndingWithTypeGuard<T extends string> = <
 
 /**
  * **endsWith**(endingIn) => (val)
- * 
+ *
  * Creates a TypeGuard which checks whether a value _ends with_ a
  * particular string literal.
  */
 export const endsWith = <
   T extends string
 >(endingIn: T): EndingWithTypeGuard<T> => <V extends Narrowable>(val: V): val is V & `${string}${T}` => {
-  return ifString(
-    val,
-    v => v.endsWith(endingIn) ? true : false,
-    v => ifNumber(
-      v, 
-      n => String(n).endsWith(endingIn) ? true : false,
-      () => false
-    )
-  ) as boolean;
+  return isString(val)
+    ?  val.endsWith(endingIn) ? true : false
+    : isNumber(val)
+      ? String(val).endsWith(endingIn) ? true : false
+      : false;
 };
