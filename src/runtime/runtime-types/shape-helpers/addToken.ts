22cf7e3f3da3ee4c471377dc42fd5fc99b57d587
Ken Snyder <ken@ken.net>
2024-07-01 15:35:49 -0700

    fix: brought back CJS build as it is needed in some places

--- src/runtime/runtime-types/shape-helpers/addToken.ts
+++ src/runtime/runtime-types/shape-helpers/addToken.ts
@@ -1,12 +1,11 @@
 import { TypeToken, TypeTokenKind } from "src/types/index";
 
 /**
- * Adds any `TypeToken` by receiving the `TypeTokenKind` and then whatever 
+ * Adds any `TypeToken` by receiving the `TypeTokenKind` and then whatever
  * parameters that type requires.
  */
 export const addToken = <
   T extends TypeTokenKind,
   TData extends readonly string[]
->(token: T, ...params: TData) => 
+>(token: T, ...params: TData) =>
   `<<${token}${params.length> 0 ? `::${params.join("::")}` : ""}>>` as TypeToken;
-
