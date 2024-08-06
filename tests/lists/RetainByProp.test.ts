f9e60f1921103ea00429d1ecd64c938fd2e2fce7
Ken Snyder <ken@ken.net>
2024-08-06 12:52:22 -0700

    feat: added RetainByProp utility

--- tests/lists/RetainByProp.test.ts
+++ tests/lists/RetainByProp.test.ts
@@ -1,12 +1,12 @@
 import {  Expect } from "@type-challenges/utils";
 import { describe, it } from "vitest";
-import { FilterByProp, GetEach, HasSameValues } from "src/types/index";
+import { GetEach, HasSameValues, RetainByProp } from "src/types/index";
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
 // standpoint so always be sure to run `tsc --noEmit` over your test files to
 // gain validation that no new type vulnerabilities have cropped up.
 
-describe("FilterByProp<TList,TComparator,TProp,[TOp]>", () => {
+describe("RetainByProp<TList,TComparator,TProp,[TOp]>", () => {
   type Data1 = [
     { id: "foo"; color: "red" },
     { id: "bar"; color: "blue" },
@@ -16,33 +16,33 @@ describe("FilterByProp<TList,TComparator,TProp,[TOp]>", () => {
   ]
 
   it("happy path", () => {
-    type NotBlue = FilterByProp<
+    type Blue = RetainByProp<
       Data1,
       "blue",
       "color"
     >;
 
-    type NoFoo = FilterByProp<
+    type Foo = RetainByProp<
       Data1,
       "foo",
       "id"
     >;
 
-    type NotStartingWithR = FilterByProp<Data1, "r", "color", "startsWith">;
+    type StartingWithR = RetainByProp<Data1, "r", "color", "startsWith">;
 
     type cases = [
       Expect<HasSameValues<
-        GetEach<NotBlue, "id">,
-        [ "foo", "basket-case", "lush"]
+        GetEach<Blue, "id">,
+        [ "bar", "baz"]
       >>,
       Expect<HasSameValues<
-        GetEach<NoFoo, "id">,
-        [ "bar", "baz", "basket-case", "lush"]
+        GetEach<Foo, "id">,
+        [ "foo" ]
       >>,
 
       Expect<HasSameValues<
-        GetEach<NotStartingWithR, "id">,
-        [ "bar", "baz" ]
+        GetEach<StartingWithR, "id">,
+        [ "foo", "basket-case", "lush" ]
       >>,
     ];
     const cases: cases = [
