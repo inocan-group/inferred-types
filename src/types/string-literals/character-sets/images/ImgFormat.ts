6424c2c37a01f2503b206e3fad25e4dc182f6cb6
Ken Snyder <ken@ken.net>
2024-08-25 17:24:47 -0700

    feat: added ImgFormat and ImgFormatWeb type utils

---
+++ src/types/string-literals/character-sets/images/ImgFormat.ts
@@ -0,0 +1,30 @@
+import { IMAGE_FORMAT_LOOKUP } from "src/constants/index";
+import { FilterByProp } from "src/types/lists";
+import { GetEach } from "src/types/lists/GetEach";
+
+/**
+ * **ImgFormat**
+ *
+ * a union of image formats named by their most typical file extension
+ *
+ * **Related:** `ImgFormatWeb`
+ */
+export type ImgFormat = GetEach<typeof IMAGE_FORMAT_LOOKUP, "ext">[number]
+
+
+/**
+ * **ImgFormatWeb**
+ *
+ * a union type of image formats typically found on the web.
+ *
+ * **Related:** `ImageFormat`
+ */
+export type ImgFormatWeb = GetEach<
+  FilterByProp<
+    typeof IMAGE_FORMAT_LOOKUP,
+    false,
+    "webFormat",
+    "equals"
+  >,
+  "ext"
+>[number]
