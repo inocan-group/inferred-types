0cae6f18d022591ac882cfd6c57dab97643d2867
Ken Snyder <ken@ken.net>
2024-08-29 11:27:52 -0700

    feat: added isHtmlElement() type guard

---
+++ src/runtime/type-guards/html/isHtmlElement.ts
@@ -0,0 +1,12 @@
+import { HtmlElement } from "src/types/string-literals";
+import { isObject } from "../isObject";
+
+
+/**
+ * **isHtmlElement**`(val)`
+ *
+ * Type guard which checks whether `val` is an `HtmlElement`.
+ */
+export const isHtmlElement = (val: unknown): val is HtmlElement => {
+  return isObject(val) && "attributes" in val && "firstElementChild" in val && "innerHTML" in val;
+}
