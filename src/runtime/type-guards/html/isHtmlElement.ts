bf4a8561d02eef9a506469bf52882a54d3646a7c
Ken Snyder <ken@ken.net>
2024-08-29 13:27:00 -0700

    fix: isHtmlElement now tests for the "right" kind of HTML element (aka, the DOM variant)

--- src/runtime/type-guards/html/isHtmlElement.ts
+++ src/runtime/type-guards/html/isHtmlElement.ts
@@ -7,6 +7,6 @@ import { isObject } from "../isObject";
  *
  * Type guard which checks whether `val` is an `HtmlElement`.
  */
-export const isHtmlElement = (val: unknown): val is HtmlElement => {
+export const isHtmlElement = (val: unknown): val is HTMLElement => {
   return isObject(val) && "attributes" in val && "firstElementChild" in val && "innerHTML" in val;
 }
