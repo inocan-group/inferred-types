90fc2bf7e5bcb2f8b37cf459c1100d4fdf205051
Ken Snyder <ken@ken.net>
2024-09-01 12:53:41 -0700

    chore: adjusted tests to use new token format for Opt<...>

--- src/runtime/type-guards/isContainer.ts
+++ src/runtime/type-guards/isContainer.ts
@@ -2,5 +2,7 @@ import { Container } from "src/types/index";
 import { isObject } from "src/runtime/index";
 
 export function isContainer<T>(value: T): value is T & Container {
-  return Array.isArray(value) || isObject(value) ? true : false;
+  return Array.isArray(value) || isObject(value)
+    ? true
+    : false;
 }
