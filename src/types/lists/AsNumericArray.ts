9c86e2999e8e25a8fead47c2c8b87245197d98e8
Ken Snyder <ken@ken.net>
2024-09-16 14:52:44 -0700

    feat: added AsNumericArray<T> utility

---
+++ src/types/lists/AsNumericArray.ts
@@ -0,0 +1,22 @@
+import { AllLiteral } from "../boolean-logic";
+import { RemoveNever } from "../containers";
+import { NumberLike } from "../numeric-literals";
+import { AsNumber } from "../type-conversion";
+
+
+/**
+ * **AsNumericArray**`<T>`
+ *
+ * Converts into a numeric array:
+ *
+ * - `number` values are proxied through
+ * - `${number}` values are converted to a number
+ * - all other types are converted to `never` and removed
+ */
+export type AsNumericArray<T> = T extends readonly unknown[]
+? RemoveNever<{
+  [K in keyof T]: T[K] extends NumberLike
+    ? AsNumber<T[K]>
+    : never
+}>
+: never;
