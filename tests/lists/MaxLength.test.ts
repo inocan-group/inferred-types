9254fb083e11469d211d58b30060b368f7a97f62
Ken Snyder <ken@ken.net>
2024-09-17 00:29:02 -0700

    feat: added MaxLength<T> util

---
+++ tests/lists/MaxLength.test.ts
@@ -0,0 +1,28 @@
+import { Equal, Expect } from "@type-challenges/utils";
+import { MaxLength } from "src/types/index";
+import { describe, it } from "vitest";
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("MaxLength<T>", () => {
+
+  it("happy path", () => {
+    type FooBar = MaxLength<["foo", "bar", "foobar", "baz"]>;
+    type FooBar2 = MaxLength<["foo", "bar", "foobar"]>;
+    type FooBar3 = MaxLength<["foobar", "bar", "foo", "baz"]>;
+    type FooBar4 = MaxLength<["foobar"]>;
+
+
+    // @ts-ignore
+    type cases = [
+      Expect<Equal<FooBar, "foobar">>,
+      Expect<Equal<FooBar2, "foobar">>,
+      Expect<Equal<FooBar3, "foobar">>,
+      Expect<Equal<FooBar4, "foobar">>,
+
+    ];
+  });
+
+});
