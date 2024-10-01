8a9a98613a56684cb1d53ae10d574c707e1f1ef0
Ken Snyder <ken@ken.net>
2024-10-01 09:28:31 -0700

    feat: added type-guard isNumberLike and type util IsTypeLike

---
+++ tests/type-guards/isNumberLike.test.ts
@@ -0,0 +1,37 @@
+import { ExpectFalse, ExpectTrue } from "@type-challenges/utils";
+import { describe, it } from "vitest";
+import { IsNumberLike } from "src/types/index"
+
+// Note: while type tests clearly fail visible inspection, they pass from Vitest
+// standpoint so always be sure to run `tsc --noEmit` over your test files to
+// gain validation that no new type vulnerabilities have cropped up.
+
+describe("IsNumberLike<T>", () => {
+
+  it("happy path", () => {
+    type T1 = IsNumberLike<42>;
+    type T2 = IsNumberLike<"42">;
+    type T3 = IsNumberLike<0>;
+    type T4 = IsNumberLike<-1>;
+    type T5 = IsNumberLike<"0">;
+    type T6 = IsNumberLike<"-1">;
+
+    type F1 = IsNumberLike<"32°">;
+    type F2 = IsNumberLike<"a">;
+
+
+    // @ts-ignore
+    type cases = [
+      ExpectTrue<T1>,
+      ExpectTrue<T2>,
+      ExpectTrue<T3>,
+      ExpectTrue<T4>,
+      ExpectTrue<T5>,
+      ExpectTrue<T6>,
+
+      ExpectFalse<F1>,
+      ExpectFalse<F2>,
+    ];
+  });
+
+});
