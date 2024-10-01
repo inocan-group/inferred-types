416f632464e330a65ef819aaf489fc943a28da68
Ken Snyder <ken@ken.net>
2024-10-01 11:14:10 -0700

    feat: added metrics and uom types

---
+++ src/types/string-literals/metrics/distance.ts
@@ -0,0 +1,33 @@
+import { DISTANCE_METRICS_LOOKUP } from "src/constants/index";
+import { OptSpace } from "../character-sets";
+
+type DistanceMetricsLookup = typeof DISTANCE_METRICS_LOOKUP;
+
+/**
+ * The singular name for metrics associated with **distance**.
+ */
+export type DistanceMetrics = {
+  [K in keyof DistanceMetricsLookup]: "name" extends keyof DistanceMetricsLookup[K]
+    ? DistanceMetricsLookup[K]["name"]
+    : never
+}[number];
+
+/**
+ * Unit of measure for metrics associated with **distance**.
+ */
+export type DistanceUom = {
+  [K in keyof DistanceMetricsLookup]: "abbrev" extends keyof DistanceMetricsLookup[K]
+    ? DistanceMetricsLookup[K]["abbrev"]
+    : never
+}[number];
+
+
+/***
+ * **Distance**
+ *
+ * A _distance_ based measurement which leads with a number and follows with
+ * a metric name.
+ *
+ * **Related:** `DistanceAbbrev`
+ */
+export type Distance = `${number}${OptSpace}${DistanceUom}`;
---
+++ src/types/string-literals/metrics/speed.ts
@@ -0,0 +1,31 @@
+import {  SPEED_METRICS_LOOKUP } from "src/constants/index";
+import { OptSpace } from "../character-sets";
+
+type SpeedMetricsLookup = typeof SPEED_METRICS_LOOKUP;
+
+/**
+ * The singular name for metrics associated with **Speed**.
+ */
+export type SpeedMetrics = {
+  [K in keyof SpeedMetricsLookup]: "name" extends keyof SpeedMetricsLookup[K]
+    ? SpeedMetricsLookup[K]["name"]
+    : never
+}[number];
+
+/**
+ * Unit of measure for metrics associated with **Speed**.
+ */
+export type SpeedUom = {
+  [K in keyof SpeedMetricsLookup]: "abbrev" extends keyof SpeedMetricsLookup[K]
+    ? SpeedMetricsLookup[K]["abbrev"]
+    : never
+}[number];
+
+/***
+ * **Speed**
+ *
+ * A _speed_ based measurement which leads with a number and follows with
+ * a speed UOM.
+ */
+export type Speed = `${number}${OptSpace}${SpeedUom}`;
+
