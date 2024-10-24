7f5d82f5b1c5ef2b26d1ddd1f4c7d10547bfeee3
Ken Snyder <ken@ken.net>
2024-10-24 16:19:04 -0700

    feat: added type guards for US states

--- src/runtime/type-guards/geo/isIso3166Alpha2.ts
+++ src/runtime/type-guards/geo/isIso3166Alpha2.ts
@@ -12,10 +12,32 @@ export const isIso3166Alpha2 = (val: unknown): val is Iso3166_1_Alpha2 => {
 }
 
 /**
+ * Type guard which checks whether `val` is a 2 character country
+ * representation found in the ISO3166-1 standard.
+ *
+ * **Alias:** `isIso3166Alpha2`
+ */
+export const isCountryCode2 = (val: unknown): val is Iso3166_1_Alpha2 => {
+  const codes = ISO3166_1.map(i => i["alpha2"]) as string[];
+  return isString(val) && codes.includes(val);
+}
+
+/**
  * Type guard which checks whether `val` is a 3 character country
  * representation found in the ISO3166-1 standard.
  */
 export const isIso3166Alpha3 = (val: unknown): val is Iso3166_1_Alpha3 => {
+  const codes = ISO3166_1.map(i => i["alpha3"]) as string[];
+  return isString(val) && codes.includes(val);
+}
+
+/**
+ * Type guard which checks whether `val` is a 3 character country
+ * representation found in the ISO3166-1 standard.
+ *
+ * **Alias:** `isIso3166Alpha3`
+ */
+export const isCountryCode3 = (val: unknown): val is Iso3166_1_Alpha3 => {
   const codes = ISO3166_1.map(i => i["alpha3"]) as string[];
   return isString(val) && codes.includes(val);
 }
@@ -30,9 +52,28 @@ export const isIso3166CountryCode = (val: unknown): val is Iso3166_1_CountryCode
 }
 
 /**
+ * Type guard which checks whether `val` is a 2 or 3 character country
+ * abbreviation found in the ISO3166-1 standard.
+ */
+export const isCountryAbbrev = (val: unknown): val is Iso3166_1_Alpha2 | Iso3166_1_Alpha3 => {
+  return isCountryCode2(val) || isCountryCode3(val)
+}
+
+/**
  * Type guard which checks whether `val` is a valid ISO3166-1 country name.
  */
 export const isIso3166CountryName = (val: unknown): val is Iso3166_1_CountryName => {
+  const codes = ISO3166_1.map(i => i["name"]) as string[];
+  return isString(val) && codes.includes(val);
+}
+
+
+/**
+ * Type guard which checks whether `val` is a valid ISO3166-1 country name.
+ *
+ * **Alias:** `isIso3166CountryName`
+ */
+export const isCountryName = (val: unknown): val is Iso3166_1_CountryName => {
   const codes = ISO3166_1.map(i => i["name"]) as string[];
   return isString(val) && codes.includes(val);
 }
