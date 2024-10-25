68d57566b47138d99fe6ce511e3dc367f722d43f
Ken Snyder <ken@ken.net>
2024-10-25 16:06:24 -0700

    chore: further refinement of repo based types and type guards

---
+++ src/types/string-literals/character-sets/urls/bitbucket.ts
@@ -0,0 +1,71 @@
+import { REPO_SOURCE_LOOKUP } from "src/constants/index";
+import { UrlsFrom } from "src/types/index";
+
+type URL = typeof REPO_SOURCE_LOOKUP["bitbucket"];
+
+/**
+ * All valid URL signatures for a Bitbucket repo.
+ */
+export type BitbucketUrl = UrlsFrom<URL>;
+
+/**
+ * A URL pattern which will show the repositories for a given organization
+ */
+export type BitbucketRepoListUrl = `https://${URL[number]}/${string}/workspace/repositories`;
+
+
+/**
+ * A URL pattern which matches any repo content in a Bitbucket repo.
+ */
+export type BitbucketRepoUrl<
+  TOrg extends string,
+  TRepo extends string
+> = `https://${URL[number]}/${TOrg}/${TRepo}`;
+
+
+/**
+ * A URL pattern which matches any repo's summary page (for a given branch)
+ */
+export type BitbucketRepoSummaryUrl<
+  TOrg extends string,
+  TRepo extends string,
+  TBranch extends string
+> = `https://${URL[number]}/${TOrg}/${TRepo}/src/${TBranch}`
+
+/**
+ * A URL pattern which matches a file in a given repo and branch
+ */
+export type BitbucketRepoFileUrl<
+  TOrg extends string,
+  TRepo extends string,
+  TBranch extends string,
+  TFilepath extends string
+> = `https://${URL[number]}/${TOrg}/${TRepo}/src/${TBranch}/${TFilepath}`
+
+export type BitbucketRepoPipelines<
+  TOrg extends string,
+  TRepo extends string,
+> = `https://${URL[number]}/${TOrg}/${TRepo}/pipelines`
+
+export type BitbucketRepoBranchList<
+  TOrg extends string,
+  TRepo extends string,
+> = `https://${URL[number]}/${TOrg}/${TRepo}/branches/`
+
+export type BitbucketRepoCommitsByBranch<
+  TOrg extends string,
+  TRepo extends string,
+  TBranch extends string,
+> = `https://${URL[number]}/${TOrg}/${TRepo}/commits/branch/${TBranch}`
+
+export type BitbucketRepoCommit<
+  TOrg extends string,
+  TRepo extends string,
+  TCommit extends string,
+> = `https://${URL[number]}/${TOrg}/${TRepo}/commits/${TCommit}`
+
+
+export type BitbucketRepoPullRequests<
+  TOrg extends string,
+  TRepo extends string,
+> = `https://${URL[number]}/${TOrg}/${TRepo}/pull-requests/${string}`
