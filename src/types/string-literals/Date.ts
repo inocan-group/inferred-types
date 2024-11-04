916743e1277ee84c01a58eafdec3c97764adbacd
Ken Snyder <ken@ken.net>
2024-11-03 16:30:12 -0800

    chore: experimenting with new path strategy for importing constants in types module

--- src/types/string-literals/Date.ts
+++ src/types/string-literals/Date.ts
@@ -2,7 +2,7 @@ import {
   NumericChar,
   NonZeroNumericChar,
 } from "src/types/index";
-import  { MONTH_ABBR, MONTH_NAME}  from "src/constants/index";
+import  { MONTH_ABBR, MONTH_NAME}  from "inferred-types";
 
 export type Year<
   T extends "strong" | "simple" = "strong"
