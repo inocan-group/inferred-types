1f203c7da071928d88578ebd7b4460370159da52
Ken Snyder <ken@ken.net>
2024-11-21 15:00:44 -0800

    fix: improved performasnce for Iso8601DateTime utility

--- src/runtime/type-guards/date-time/isDateTime.ts
+++ src/runtime/type-guards/date-time/isDateTime.ts
@@ -1,18 +1,17 @@
 import { Iso8601DateTime } from "inferred-types/dist/types/index";
 import { isNumberLike, isString, stripUntil, } from "src/runtime/index";
 
-
+/**
+ * Type guard which validates that `val` is a valid ISO8601 DateTime string.
+ */
 export const isIsoDateTime = (val: unknown): val is Iso8601DateTime => {
   if (isString(val)) {
-
-    return isString(val) &&
-      val.includes(":") &&
+    return val.includes(":") &&
       val.includes("-") &&
       val.split("-").length === 3 &&
       val.split(":").length > 1 &&
       (!val.includes("Z") || isNumberLike(val.slice(-1)) || val.endsWith("Z")) &&
       (!val.includes("Z") || ["+","-"].includes(stripUntil(val, "Z").slice(1,2)) || val.endsWith("Z"))
-
   }
 
   return false;
