ded8ff6dcc283397741003dbf5cf110d366a1c93
Ken Snyder <ken@ken.net>
2024-11-22 11:36:53 -0800

    feat: added isToday(), isTomorrow(), and isYesterday() type guards

---
+++ src/runtime/type-guards/date-time/isYesterday.ts
@@ -0,0 +1,32 @@
+import {  getYesterday, isDate, isLuxonDateTime } from "src/runtime/index";
+import { isIsoExplicitDate, isMoment, isString, stripAfter } from "src/runtime/index";
+import { Iso8601Date, LuxonJS, MomentJs } from "src/types/index";
+
+
+/**
+* **isYesterday`(val)`**
+* A type guard which validates whether the passed in value _represents_ yesterday's date
+* as either a:
+*
+*   - [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) date string, or
+*   - [Moment.js](https://momentjs.com/docs/#/displaying/)  DateTime object, or
+*   - [Luxon](https://moment.github.io/luxon/#/?id=luxon) DateTime object
+*/
+export const isYesterday = (
+  test: unknown
+): test is Iso8601Date<"explicit"> | LuxonJS["DateTime"] | MomentJs => {
+	if (isString(test)) {
+		const justDate = stripAfter(test, "T");
+		return isIsoExplicitDate(justDate) && justDate === getYesterday();
+	} else if (isMoment(test) || isDate(test)) {
+		return (
+		  stripAfter(test.toISOString(), "T") === getYesterday()
+		);
+	} else if (isLuxonDateTime(test)) {
+		return (
+		  stripAfter(test.toISO(), "T") === getYesterday()
+		);
+	}
+
+	return false;
+};
