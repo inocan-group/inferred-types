e2fc5969bf04f2dd04c1bb68efcede9c56839e9b
Ken Snyder <ken@ken.net>
2024-11-22 15:24:14 -0800

    chore: address linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -45,7 +45,7 @@ export default ts.config({
             "error",
             {
               "args": "all",
-              "argsIgnorePattern": ["^_", "cases"],
+              "argsIgnorePattern": "^_",
               "caughtErrors": "all",
               "caughtErrorsIgnorePattern": "^_",
               "destructuredArrayIgnorePattern": "^_",
--- src/runtime/type-guards/date-time/isLuxonDateTime.ts
+++ src/runtime/type-guards/date-time/isLuxonDateTime.ts
@@ -9,19 +9,19 @@ export const isLuxonDateTime = (val: unknown): val is LuxonJs["DateTime"] => {
   return isObject(val) &&
     typeof val === 'object' &&
     val !== null &&
-    'isValid' in val &&
-    'invalidReason' in val &&
-    'invalidExplanation' in val &&
-    'toISO' in val &&
-    'toFormat' in val &&
-    'toMillis' in val &&
-    'year' in val &&
-    'month' in val &&
-    'day' in val &&
-    'hour' in val &&
-    'minute' in val &&
-    'second' in val &&
-    'millisecond' in val &&
-    typeof (val as any).isValid === 'boolean' &&
-    typeof (val as any).toISO === 'function'
+    "isValid" in val &&
+    "invalidReason" in val &&
+    "invalidExplanation" in val &&
+    "toISO" in val &&
+    "toFormat" in val &&
+    "toMillis" in val &&
+    "year" in val &&
+    "month" in val &&
+    "day" in val &&
+    "hour" in val &&
+    "minute" in val &&
+    "second" in val &&
+    "millisecond" in val &&
+    typeof (val as any).isValid === "boolean" &&
+    typeof (val as any).toISO === "function"
 }
