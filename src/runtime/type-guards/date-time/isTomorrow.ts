a6528cff0136719758c711c60854f804a8388960
Ken Snyder <ken@ken.net>
2024-11-23 15:17:38 -0800

    fix: improve the returned types for the isToday(), isTomorrow(), and isYesterday() type guards

--- src/runtime/type-guards/date-time/isTomorrow.ts
+++ src/runtime/type-guards/date-time/isTomorrow.ts
@@ -1,7 +1,30 @@
 import {  getTomorrow,  isDate, isLuxonDateTime } from "src/runtime/index";
 import { isIsoExplicitDate, isMoment, isString, stripAfter } from "src/runtime/index";
-import { Iso8601Date, LuxonJs, MomentJs } from "src/types/index";
+import {
+  IsJsDate,
+  IsLuxonDateTime,
+  IsMoment,
+  Iso8601Date,
+  Iso8601DateTime,
+  LuxonJs,
+  MomentJs
+} from "src/types/index";
 
+type DateType<T> = T extends Iso8601Date<"explicit">
+? Iso8601Date<"explicit">
+: T extends Iso8601Date<"implicit">
+? Iso8601Date<"implicit">
+: T extends Iso8601DateTime
+? Iso8601DateTime
+: IsMoment<T> extends true
+? MomentJs
+: IsJsDate<T> extends true
+? Date
+: IsLuxonDateTime<T> extends true
+? LuxonJs["DateTime"]
+: T extends string
+? Iso8601Date | Iso8601DateTime
+: never;
 
 /**
 * **isTomorrow`(val)`**
@@ -12,9 +35,9 @@ import { Iso8601Date, LuxonJs, MomentJs } from "src/types/index";
 *   - [Moment.js](https://momentjs.com/docs/#/displaying/)  DateTime object, or
 *   - [Luxon](https://moment.github.io/luxon/#/?id=luxon) DateTime object
 */
-export const isTomorrow = (
-  test: unknown
-): test is Iso8601Date<"explicit"> | LuxonJs["DateTime"] | MomentJs => {
+export const isTomorrow = <T>(
+  test: T
+): test is T & DateType<T> => {
 	if (isString(test)) {
 		const justDate = stripAfter(test, "T");
 		return isIsoExplicitDate(justDate) && justDate === getTomorrow();
