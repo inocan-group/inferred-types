3d2d5102e38e4b4b988f3af54fe2fa52bdd30903
Ken Snyder <ken@ken.net>
2024-11-26 11:10:17 -0800

    feat: added DayOfWeek and DayOfWeekFull union types

---
+++ src/types/string-literals/Calendar.ts
@@ -0,0 +1,14 @@
+
+/**
+* The days of the week (abbreviated)
+*
+* **Related:** `DayofWeekFull`
+*/
+export type DayOfWeek = "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat";
+
+/**
+* The days of the week (fully spelled out)
+*
+* **Related:** `DayofWeek`
+*/
+export type DayofWeekFull = "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday";
