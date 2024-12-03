dbff80804f3d1ddf25b26e6d83c777df15b7f892
Ken Snyder <ken@ken.net>
2024-12-02 18:12:29 -0800

    chore: added MDN links to CSS types

--- src/types/string-literals/character-sets/css/definition.ts
+++ src/types/string-literals/character-sets/css/definition.ts
@@ -20,6 +20,7 @@ import {
   CssAppearance,
   CssBackdropFilter,
   CssContent,
+  CssCursor,
   CssFlex,
   CssFlexBasis,
   CssFlexDirection,
@@ -51,7 +52,9 @@ import { CssTransformProperties } from "./transform";
 
 
 type _CssDefinition = {
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/appearance) */
   appearance?: CssAppearance;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/backdrop-filter) */
   "backdrop-filter"?: CssBackdropFilter;
   /**
    * The valid values for the
@@ -59,20 +62,29 @@ type _CssDefinition = {
    * property in CSS.
    */
   display?: CssDisplay;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/position) */
   position?: CssPosition;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/aspect-ratio) */
   "aspect-ratio"?: CssAspectRatio;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/opacity) */
   opacity?: CssOpacity;
-  overflowX?: CssOverflowX;
-  overflowY?: CssOverflowY;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/gap) */
   gap?: CssGap;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex) */
   flex?: CssFlex;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex-grow) */
   "flex-grow"?: CssFlexGrow;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex-shrink) */
   "flex-shrink"?: CssFlexShrink;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex-direction) */
   "flex-direction"?: CssFlexDirection;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex-flow) */
   "flex-flow"?: CssFlexFlow;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/flex-basis) */
   "flex-basis"?: CssFlexBasis;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/float) */
   float?: CssFloat;
 
   /**
@@ -91,85 +103,120 @@ type _CssDefinition = {
    * If the writing mode is vertically oriented, the value of block-size relates to the width of
    * the element; otherwise, it relates to the height of the element. A related property is
    * inline-size, which defines the other dimension of the element.
-   */
+   *
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/block-size)
+  */
   "block-size"?: CssSizing;
 
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/border-size) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/border-size) */
   "border-size"?: CssSizing;
 
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/content) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/content) */
   content?: CssContent;
 
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/color) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/color) */
   color?: CssColor;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/height) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/cursor) */
+  cursor?: CssCursor | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/height) */
   height?: CssSizing;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/width) */
   width?: CssSizing;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/perspective) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/perspective) */
   perspective?: CssSizingLight | "none" | CssGlobal;
   "perspective-origin"?: CssPerspectiveOrigin;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode) */
   "mix-blend-mode": CssMixBlendMode;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/letter-spacing) */
   "letter-spacing"?: CssLetterSpacing;
   /**
    * The lighting-color CSS property defines the color of the light source for the
    * <feDiffuseLighting> and <feSpecularLighting> SVG lighting filter primitives within an
    * SVG <filter>. If present, it overrides the element's lighting-color attribute.
    *
-   * - [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/lighting-color)
+   * - [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/lighting-color)
    */
   "lighting-color"?: CssColor;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit) */
   "object-fit"?: CssObjectFit;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/object-position) */
   "object-position"?: CssObjectPosition;
   order?: `${number}` | CssGlobal;
-
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/image-orientation) */
   "image-orientation"?: CssImageOrientation;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/image-rendering) */
   "image-rendering"?: CssImageRendering;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/image-resolution) */
   "image-resolution"?: CssImageResolution;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/hanging-punction) */
   "hanging-punctuation"?: CssHangingPunctuation;
 
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-height) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-height) */
   "min-height"?: CssSizing;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-width) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-width) */
   "min-width"?: CssSizing;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-block-size) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-block-size) */
   "min-block-size"?: CssSizing;
-  /** [Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-inline-size) */
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/min-inline-size) */
   "min-inline-size"?: CssSizing;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/stop-color) */
   "stop-color"?: CssColorLight  | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/stop-opacity) */
   "stop-opacity"?: `${number}` | `${number}%` | CssGlobal;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/tab-size) */
   "tab-size"?: `${number}` | CssSizingLight | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/table-layout) */
   "table-layout"?: "auto" | "fixed" | CssGlobal;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/translate) */
   translate?: CssTranslate | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/quotes) */
   quotes?: string;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/r) */
   r?: CssSizingLight | CssGlobal;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/pointer-events) */
   "pointer-events"?: PointerEvent | CssGlobal;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/resize) */
   resize?: "both" | "horizontal" | "vertical" | "none" | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/rotate) */
   rotate?: CssRotation | `x ${CssRotation}` | `y ${CssRotation}` | `z ${CssRotation}` | CssGlobal;
   "row-gap"?: CssSizing;
+
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/rx) */
   rx?: CssSizing;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/ry) */
   ry?: CssSizing;
 
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/x) */
   x?: CssSizing;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/y) */
   y?: CssSizing;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/white-space) */
   "white-space"?: CssWhiteSpace;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/white-space-collapse) */
   "white-space-collapse"?: CssWhiteSpaceCollapse;
-  "windows"?: string;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/widows) */
+  "widows"?: string;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/will-change) */
   "will-change"?: string;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/word-break) */
   "word-break"?: CssWordBreak;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/word-spacing) */
   "word-spacing"?: "normal" | CssSizing;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/writing-mode) */
   "writing-mode"?: CssWritingMode;
 
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/z-index) */
   "z-index"?: `${number}` | "auto" | CssGlobal;
+  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/CSS/zoom) */
   "zoom"?: `${number}` | `${number}%` | "normal" | "reset" | CssGlobal;
 }
 & CssBorderProperties
