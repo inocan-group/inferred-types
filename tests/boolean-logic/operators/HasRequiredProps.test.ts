a1db881f4deb748b2bbf466b77f2631e3da91a93
Ken Snyder <ken@ken.net>
2024-12-08 16:51:25 -0800

    fix: making the tsconfig files across project more consistent

--- tests/boolean-logic/operators/HasRequiredProps.test.ts
+++ tests/boolean-logic/operators/HasRequiredProps.test.ts
@@ -1,10 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Equal, Expect, ExpectFalse, ExpectTrue } from "@type-challenges/utils";
-import {  HasRequiredProps } from "inferred-types/types";
+import {  EmptyObject, HasRequiredProps } from "inferred-types/types";
 import { describe, it } from "vitest";
-
-// Note: while type tests clearly fail visible inspection, they pass from Vitest
-// standpoint so always be sure to run `tsc --noEmit` over your test files to
-// gain validation that no new type vulnerabilities have cropped up.
 
 describe("HasRequiredProps<T>", () => {
 
@@ -13,12 +10,12 @@ describe("HasRequiredProps<T>", () => {
     type T2 = HasRequiredProps<{foo: 1; bar?: 2}>;
 
     type F1 = HasRequiredProps<{foo?: 1; bar?: 2}>;
-    type F2 = HasRequiredProps<{}>;
+    type F2 = HasRequiredProps<EmptyObject>;
 
     type Wide = HasRequiredProps<Record<string, unknown>>;
 
     // @ts-ignore
-    type cases = [
+    type _cases = [
       ExpectTrue<T1>,
       ExpectTrue<T2>,
       ExpectFalse<F1>,
