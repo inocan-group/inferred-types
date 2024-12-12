bb4a688b24158455b8ee8b508bfd514db6324b0c
Ken Snyder <ken@ken.net>
2024-12-12 11:04:57 -0800

    chore: improved build script and added watch script

--- modules/types/tsup.config.ts
+++ modules/types/tsup.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from "tsup";
+import { defineConfig, type Format } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: { entry: ["src/index.ts"] },
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  treeshake: true,
-  outExtension: ({ format }) => {
-    return {
-      js: format === "cjs" ? ".cjs" : ".js",
-      dts: format === "cjs" ? ".cts" : ".ts",
-    };
-  },
-  tsconfig: "./tsconfig.tsup.json",
-});
+function config(format: Format) {
+  return defineConfig({
+    entry: ["src/index.ts"],
+    format,
+    dts: format === "cjs" ? { entry: ["src/index.ts"] } : false,
+    sourcemap: true,
+    clean: false,
+    outExtension: ({ format }) => {
+      return {
+        js: format === "cjs" ? ".cjs" : ".js",
+        dts: ".ts",
+      };
+    },
+    tsconfig: "./tsconfig.tsup.json",
+  });
+}
+
+export default [config("esm"), config("cjs")];
