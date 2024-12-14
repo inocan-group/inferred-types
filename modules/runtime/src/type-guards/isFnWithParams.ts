5d2a57d7f1617b14cc931b2c20242dfbd3be8aa5
Ken Snyder <ken@ken.net>
2024-12-14 11:07:40 -0800

    feat: improved functionality of isFnWithParams(fn, ...x) where x can be either the expected types or a number representing the number of parameters expected.

--- modules/runtime/src/type-guards/isFnWithParams.ts
+++ modules/runtime/src/type-guards/isFnWithParams.ts
@@ -1,28 +1,41 @@
 import type {
   AsNarrowingFn,
+  IsLiteral,
   SimpleToken,
   SimpleType,
+  Tuple,
   TypedFunction,
 } from "inferred-types/types";
 
 /**
- * **isFnWithDict**(input)
+ * **isFnWithParams**(input, [...params | count])
  *
  * Type guard which checks whether a give variable is a function
  * which contains at least one parameter.
  *
- * - if you set the parameter types, it will check that precisely the
- * correct amount are set
+ * - the optional second parameter may be used to express the **types** of
+ * the expected parameters or just the expected **count** of parameters
+ * - if you set the parameter types, it will check that the
+ * correct amount of parameters are set but can only _assume_ the types of
+ * the parameters are as you have expressed
  */
-export function isFnWithParams<T, P extends readonly SimpleToken[]>(
+export function isFnWithParams<T, P extends (readonly SimpleToken[]) | [number]>(
   input: T,
   ...params: P
 ): input is T & AsNarrowingFn<
-  P["length"] extends 0
-    ? [any, ...any[]]
-    : {
-        [K in keyof P]: SimpleType<P[K]>
-      },
+  "length" extends keyof T
+    ? T["length"] extends 0
+      ? IsLiteral<P["length"]> extends true
+        ? Tuple<any, P["length"]>
+        : [any, ...any[]]
+      : [any, ...any[]]
+    : P extends readonly SimpleToken[]
+      ? {
+          [K in keyof P]: SimpleType<P[K]>
+        }
+      : P extends [number]
+        ? Tuple<any, P[0]>
+        : never,
   T extends TypedFunction ? ReturnType<T> : unknown
 > {
   return params.length === 0
