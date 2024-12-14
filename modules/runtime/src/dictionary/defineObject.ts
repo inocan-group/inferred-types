66d90c4f425eebd9dbf3b671ee6c5f2ebfd18a73
Ken Snyder <ken@ken.net>
2024-12-14 08:16:13 -0800

    feat: defineObject(defn,...) now allows the optional declaration of certain props to be optional.

--- modules/runtime/src/dictionary/defineObject.ts
+++ modules/runtime/src/dictionary/defineObject.ts
@@ -1,4 +1,11 @@
-import type { DefineObject, FromDefn } from "inferred-types/types";
+import type { DefineObject, FromDefineObject, MakeKeysOptional } from "inferred-types/types";
+
+
+type Returns<T extends DefineObject, P extends readonly (keyof T & string)[]> = P["length"] extends 0
+? FromDefineObject<T>
+: MakeKeysOptional<T,P> extends DefineObject
+  ? FromDefineObject<MakeKeysOptional<T,P>>
+  : never;
 
 /**
  * Takes an object definition where the values are either
@@ -7,6 +14,9 @@ import type { DefineObject, FromDefn } from "inferred-types/types";
  * In both cases the runtime type is left unchanged but the
  * type is converted to represent the designed object shape.
  */
-export function defineObject<T extends DefineObject>(defn: T): FromDefn<T> {
-  return defn as unknown as FromDefn<T>;
+export function defineObject<T extends DefineObject, P extends readonly (keyof T & string)[]>(
+  defn: T,
+  ...optProps: P
+): Returns<T,P> {
+  return defn as unknown as Returns<T,P>;
 }
