f5c9ce70f9d5548da0e3603cc7a101f022f55364
Ken Snyder <ken@ken.net>
2024-12-15 14:46:05 -0800

    feat: added "unset" constant which can be used with `isSet()`

--- modules/runtime/src/literals/unset.ts
+++ modules/runtime/src/literals/unset.ts
@@ -1,18 +1,3 @@
-// import { Unset, ShapeCallback } from "inferred-types/types";
-// import { ShapeApiImplementation, handleEscapeApi, isFunction } from "inferred-types/runtime";
+import { Unset } from "inferred-types/types"
 
-export const unset = "NOT IMPLEMENTED" as const;
-
-/**
- * **unset**`([type])`
- *
- * Allows you to easily create the `Unset` type while providing a tokenized runtime
- * value for the `type` property.
- */
-// export const unset = <T extends ShapeCallback | Unset = Unset>(type?: T) => {
-//     if(isFunction(type)) {
-//       const _userResponse = handleEscapeApi(ShapeApiImplementation);
-
-//     }
-
-// }
+export const unset = "<<unset>>" as unknown as Unset;
