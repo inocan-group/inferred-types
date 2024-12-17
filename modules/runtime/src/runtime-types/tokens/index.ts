90c02618cc4cfefd5dcfc4d08673ba0c06669da6
Ken Snyder <ken@ken.net>
2024-12-16 17:55:12 -0800

    chore: cleanup

--- modules/runtime/src/runtime-types/tokens/index.ts
+++ modules/runtime/src/runtime-types/tokens/index.ts
@@ -1,5 +1,5 @@
 export * from "./brands";
 export * from "./createTypeToken";
+export * from "./fromDefineObject";
 export * from "./getTokenKind";
 export * from "./simpleToken";
-export * from "./fromDefineObject";
