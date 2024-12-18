7fbc25e4a67e6ac010c37ce26fc486300d3b15fd
Ken Snyder <ken@ken.net>
2024-12-18 14:59:58 -0800

    feat: added variant to narrowObjectTo() called narrowObjectToType()

--- modules/runtime/src/dictionary/narrowObjectTo.ts
+++ modules/runtime/src/dictionary/narrowObjectTo.ts
@@ -1,4 +1,5 @@
 import type {
+  AnyObject,
   ConstrainedObjectIdentity,
   ConstrainObject,
   DefineObject,
@@ -20,7 +21,7 @@ import type {
  * const obj = fn({foo: "foo", bar: 42});
  * ```
  *
- * **Related:** `ConstrainedObjectIdentity`
+ * **Related:** `ConstrainedObjectIdentity`, `narrowObjectToType`
  */
 export function narrowObjectTo<
   TDefn extends DefineObject,
@@ -35,3 +36,32 @@ export function narrowObjectTo<
 
   return fn as ConstrainedObjectIdentity<FromDefineObject<TDefn>>;
 }
+
+/**
+ * **narrowObjectToType**`<Constraint>() => (obj) => obj
+ *
+ * Ensures that this identity function extracts a narrow type
+ * definition from whatever is passed into `obj` while ensuring
+ * that `obj` conform to the broader type definition `constraint`.
+ *
+ * ```ts
+ * const fn = narrowObjectToType<{foo: "string", bar: "Opt<number>"}>();
+ * // {foo: "foo"; bar: 42}
+ * const obj = fn({foo: "foo", bar: 42});
+ * ```
+ *
+ * **Related:** `ConstrainedObjectIdentity`, `narrowObjectTo`
+ */
+export const narrowObjectToType = <TDefn extends AnyObject>(): ConstrainedObjectIdentity<TDefn> => {
+    const fn = <
+    T extends Record<string, N>,
+    N extends Narrowable,
+    Constraint extends TDefn,
+  >(
+    obj: T & ConstrainObject<T, Constraint>,
+  ) => obj as ExpandDictionary<T>;
+
+  return fn as ConstrainedObjectIdentity<TDefn>;
+
+  }
+
