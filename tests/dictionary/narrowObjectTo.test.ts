09e2197eb915c65525578b427addb98d05b07053
Ken Snyder <ken@ken.net>
2024-12-18 14:59:58 -0800

    feat: added variant to narrowObjectTo() called narrowObjectToType()

--- tests/dictionary/narrowObjectTo.test.ts
+++ tests/dictionary/narrowObjectTo.test.ts
@@ -1,5 +1,5 @@
 import { Equal, Expect } from "@type-challenges/utils";
-import { narrowObjectTo } from "inferred-types/runtime";
+import { narrowObjectTo, narrowObjectToType } from "inferred-types/runtime";
 import { describe, expect, it } from "vitest";
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
@@ -12,13 +12,19 @@ describe("narrowObjectTo(constraint) -> (obj) -> obj", () => {
     const a = narrowObjectTo({foo: "string", bar: "Opt<number>"})
     const b = a({foo: "foo", bar: 42});
 
+    const a2 = narrowObjectToType<{foo: string; bar?: number}>()
+    const b2 = a2({foo: "foo", bar: 42});
+
     expect(b).toEqual({foo: "foo", bar: 42})
 
 
     // @ts-ignore
     type cases = [
-      Expect<Equal<typeof b, { foo: "foo"; bar: 42 }>>
+      Expect<Equal<typeof b, { foo: "foo"; bar: 42 }>>,
+      Expect<Equal<typeof b2, { foo: "foo"; bar: 42 }>>
     ];
   });
 
 });
+
+
