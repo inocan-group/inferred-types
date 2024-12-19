ff0fa5fce858f064d4b79a8a8b5a6d658ff856c2
Ken Snyder <ken@ken.net>
2024-12-18 17:29:57 -0800

    fix: don't allow the narrow object to be pushed through narrowObjectToType() when missing required props on constraint

--- modules/types/src/dictionary/ConstrainObject.ts
+++ modules/types/src/dictionary/ConstrainObject.ts
@@ -1,18 +1,57 @@
 import type {
   AnyObject,
+  As,
+  ContainsAll,
   ExpandDictionary,
+  Filter,
+  IsOptional,
+  Keys,
   Narrowable,
+  ObjectKey,
+  RequiredKeys,
+  RequiredKeysTuple,
+  SetKeysTo,
 } from "inferred-types/types";
+
+
+type HasRequiredKeys<
+  TObj extends readonly ObjectKey[],
+  TConstraint extends readonly ObjectKey[]
+> = Filter<TConstraint, TObj[number]>["length"] extends 0
+  ? true
+  : false;
+
+type MissingKeys<
+  TObj extends AnyObject,
+  TConstraint extends AnyObject
+> = Filter<
+  RequiredKeysTuple<TConstraint>,
+  keyof TObj
+> ;
+
+type X = RequiredKeysTuple<{foo: string; bar?: number}>;
+type Y = As<Keys<{bar: 23}>, readonly ObjectKey[]>;
+
+type Z = MissingKeys<{bar: 23}, {foo: string; bar?: number}>;
 
 /**
  * Helper type to infer narrow types while constraining to a broad type.
  */
 export type ConstrainObject<
-  TObj,
-  TConstraint,
-> = {
-  [K in keyof TObj]: K extends keyof TConstraint ? TObj[K] & TConstraint[K] : never;
-};
+  TObj extends AnyObject,
+  TConstraint extends AnyObject,
+> = HasRequiredKeys<
+  As<Keys<TObj>, readonly ObjectKey[]>,
+  RequiredKeysTuple<TConstraint>
+  > extends true
+? {
+  [K in keyof TObj]: K extends keyof TConstraint
+    ? TObj[K] & TConstraint[K]
+    : never;
+}
+: TConstraint
+
+;
 
 /**
  * A function which will accept any object that meets the
@@ -22,9 +61,19 @@ export type ConstrainObject<
  */
 export type ConstrainedObjectIdentity<
   Constraint extends AnyObject,
-> = <
-  T extends Record<string, N>,
-  N extends Narrowable,
-  TConstraint extends Constraint,
->(obj: T & ConstrainObject<T, TConstraint>
-) => ExpandDictionary<T>;
+> = (
+  <
+    T extends Record<string, N>,
+    N extends Narrowable,
+    TConstraint extends Constraint,
+  >(
+    obj: T & ConstrainObject<T, TConstraint>
+  ) => ExpandDictionary<T>
+) & (
+  {
+    cb: <
+      TCb extends (obj: ReturnType<ConstrainedObjectIdentity<Constraint>>) => unknown,
+    >(cb: TCb
+    ) => ReturnType<TCb>;
+  }
+);
