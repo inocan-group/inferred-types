42bc4367b3bb9e5d9df2e2d2a0eaa359af68c559
Ken Snyder <ken@ken.net>
2024-12-21 15:51:57 -0800

    feat: added isSetContainer() and isMap() type guards

--- modules/runtime/src/type-guards/isUnset.ts
+++ modules/runtime/src/type-guards/isUnset.ts
@@ -11,16 +11,3 @@ import { isObject } from "./isObject";
 export function isUnset(val: unknown): val is Unset {
   return isObject(val) && val.kind === "Unset";
 }
-
-/**
- * **isSet**`(val)`
- *
- * Type guard which validates that the value passed in is **not** `Unset`.
- *
- * **Related:** `isUnset()`
- */
-export function isSet<T>(val: T): val is Exclude<T, Unset> {
-  return isObject(val)
-    ? val.kind !== "Unset"
-    : true;
-}
