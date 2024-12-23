fedb703af8d324fa58c7338e565f6140848c99a2
Ken Snyder <ken@ken.net>
2024-12-22 21:33:56 -0800

    feat: added base type `Variable` and more substantive type check with boolean operator `IsVariable`

---
+++ modules/types/src/base-types/Variable.ts
@@ -0,0 +1,36 @@
+import type {
+  AfterFirst,
+  AlphaChar,
+  AlphaNumericChar,
+  Chars,
+  First,
+  VariableChar,
+} from "inferred-types/types";
+
+/**
+ * **Variable**
+ *
+ * the **Variable** type reinforces that a variable:
+ *
+ * - start with an alpha character (upper or lower)
+ * - end with an alphanumeric character
+ *
+ * Further type safety would become too expensive.
+ */
+export type Variable = `${AlphaChar}${string}${AlphaNumericChar}`;
+
+type ValidChars<T extends readonly string[]> = [] extends T
+  ? true
+  : First<T> extends VariableChar
+    ? ValidChars<AfterFirst<T>>
+    : false;
+
+/**
+ * Boolean operator which returns `true` when `T` is a valid variable
+ * name.
+ */
+export type IsVariable<T extends string> = T extends Variable
+  ? ValidChars<Chars<T>> extends true
+    ? true
+    : false
+  : false;
