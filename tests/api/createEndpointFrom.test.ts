0e0f5f2c42fbbf4761d480509f0cfc035efbfda6
Ken Snyder <ken@ken.net>
2024-12-28 13:56:25 -0800

    fix: fixed premature test

--- tests/api/createEndpointFrom.test.ts
+++ tests/api/createEndpointFrom.test.ts
@@ -1,24 +1,13 @@
 import { } from "@type-challenges/utils";
-import { describe, it } from "vitest";
-import { createEndpointsFrom } from "../../modules/runtime/src/api/createEndpoint";
+import { describe, } from "vitest";
+
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
 // standpoint so always be sure to run `tsc --noEmit` over your test files to
 // gain validation that no new type vulnerabilities have cropped up.
 
-describe("createEndpointFrom()", () => {
-
-  it("happy path", () => {
-    const build = createEndpointsFrom("https://api.openai.com/v1");
-    const end = build
-      .delete("/models/<model>", "remove shit")
-      .get("")
+describe.skip("createEndpointFrom()", () => {
 
 
-    // @ts-ignore
-    type cases = [
-      /** type tests */
-    ];
-  });
 
 });
