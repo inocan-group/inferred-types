9e89043758fe768c00ebf88d6fa11663d08bf925
Ken Snyder <ken@ken.net>
2024-12-30 14:55:49 -0800

    feat: improved the stripTrailing() utility to accept undefined values in addition to strings

--- modules/runtime/src/literals/stripTrailing.ts
+++ modules/runtime/src/literals/stripTrailing.ts
@@ -1,5 +1,14 @@
 import type { StripTrailing, TupleToUnion } from "inferred-types/types";
-import { isNumber } from "inferred-types/runtime";
+import { isNumber, isUndefined } from "inferred-types/runtime";
+
+type Returns<
+  T extends string | number | undefined,
+  U extends readonly (string | number)[],
+> = T extends undefined
+  ? undefined
+  : T extends string | number
+    ? StripTrailing<T, TupleToUnion<U>>
+    : never;
 
 /**
  * **stripTrailing**(content, strip)
@@ -8,12 +17,16 @@ import { isNumber } from "inferred-types/runtime";
  * removed if it exists and that strong typing is preserved.
  */
 export function stripTrailing<
-  T extends string | number,
+  T extends string | number | undefined,
   U extends readonly (string | number)[],
 >(
   content: T,
   ...strip: U
-): StripTrailing<T, TupleToUnion<U>> {
+): Returns<T, U> {
+  if (isUndefined(content)) {
+    return undefined as Returns<T, U>;
+  }
+
   let output: string = String(content);
 
   for (const s of strip) {
@@ -24,5 +37,5 @@ export function stripTrailing<
 
   return (
     isNumber(content) ? Number(output) : output
-  ) as unknown as StripTrailing<T, TupleToUnion<U>>;
+  ) as unknown as Returns<T, U>;
 }
