b528f55594ed4c6e3933b5bb024343388044f76e
Ken Snyder <ken@ken.net>
2025-01-04 04:48:50 -0800

    fix: greatly simplified mergeObjects() runtime utility

--- tests/type-conversions/MergeObjects.test.ts
+++ tests/type-conversions/MergeObjects.test.ts
@@ -1,6 +1,7 @@
 import { Equal, Expect } from "@type-challenges/utils";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { MergeObjects } from "inferred-types";
+import {  mergeObjects } from "inferred-types/runtime";
 
 // Note: while type tests clearly fail visible inspection, they pass from Vitest
 // standpoint so always be sure to run `tsc --noEmit` over your test files to
@@ -13,15 +14,12 @@ describe("MergeObjects<A,B>", () => {
     type T1 = MergeObjects<{ foo: 1; bar: 2 }, { bar: 4; baz: "howdy" }>;
     type T2 = MergeObjects<{ bar: 4; baz: "howdy" }, { foo: 1; bar: 2 }>;
 
-
+    // @ts-ignore
     type cases = [
       Expect<Equal<T1, {foo: 1; bar: 4; baz: "howdy"}>>,
       Expect<Equal<T2, {foo: 1; bar: 2; baz: "howdy"}>>,
     ];
 
-    const cases: cases = [
-      true, true,
-    ];
   });
 
 
@@ -35,5 +33,24 @@ describe("MergeObjects<A,B>", () => {
 
   });
 
+
+});
+
+describe("mergeObjects(a,b)", () => {
+
+  it("happy path", () => {
+    const t1 = mergeObjects({ foo: 1, bar: 2 }, { bar: 4, baz: "howdy" });
+    const t2 = mergeObjects({ bar: 4, baz: "howdy" }, { foo: 1, bar: 2 });
+
+    expect(t1).toEqual({ foo:1, bar: 4, baz: "howdy"});
+    expect(t2).toEqual({ foo:1, bar: 2, baz: "howdy"});
+
+    // @ts-ignore
+    type cases = [
+      Expect<Equal<typeof t1, {foo: 1; bar: 4; baz: "howdy"}>>,
+      Expect<Equal<typeof t2, {foo: 1; bar: 2; baz: "howdy"}>>,
+    ];
+
+  });
 
 });
