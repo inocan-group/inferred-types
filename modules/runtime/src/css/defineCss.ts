b88c555d4e6702f2e4b8bf8ba21a7a430b57d857
Ken Snyder <ken@ken.net>
2025-01-05 14:01:09 -0800

    feat: exporting cssFromDefinition() runtime utility

--- modules/runtime/src/css/defineCss.ts
+++ modules/runtime/src/css/defineCss.ts
@@ -1,17 +1,25 @@
 import type { CssDefinition } from "inferred-types/types";
 import { createFnWithProps, ensureTrailing } from "inferred-types/runtime";
 
-function createCss<T extends CssDefinition>(defn: T, indent = "") {
+/**
+ * **cssFromDefinition**`(defn, [indent], [inline])`
+ *
+ * converts a `CssDefinition` into a CSS string
+ */
+export function cssFromDefinition<
+  T extends CssDefinition,
+>(defn: T, indent = "", inline: boolean = false) {
+  const nextDefn = inline ? " " : "\n";
   return Object.keys(defn)
-    .map(key => `${indent}${key}: ${ensureTrailing(defn[key as keyof typeof defn] as string, ";")}\n`)
+    .map(key => `${indent}${key}: ${ensureTrailing(defn[key as keyof typeof defn] as string, ";")}${nextDefn}`)
     .join("");
 }
 
 export function defineCss<T extends CssDefinition>(defn: T) {
   const fn = <S extends string>(selector?: S) => {
     return selector
-      ? `${selector} {\n${createCss(defn, "  ")}}\n`
-      : createCss(defn);
+      ? `${selector} {\n${cssFromDefinition(defn, "  ")}}\n`
+      : cssFromDefinition(defn);
   };
   return createFnWithProps(
     fn,
