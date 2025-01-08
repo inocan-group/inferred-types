f9ff3c0c45ad776daeaffb08cf235ad641070846
Ken Snyder <ken@ken.net>
2025-01-08 12:29:36 -0800

    fix: fixed isDefined(val) type guard

--- tests/lists/filterUndefined.test.ts
+++ tests/lists/filterUndefined.test.ts
@@ -1,14 +1,14 @@
 import { Equal, Expect } from "@type-challenges/utils";
 import { describe, expect, it } from "vitest";
-import { filterUndefined, isEmpty } from "inferred-types/runtime";
+import { filterUndefined, isDefined, isEmpty } from "inferred-types/runtime";
 
 describe("filterUndefined", () => {
 
   it("happy path", () => {
     const t1 = filterUndefined(1, 2, undefined, "foo" as string, "")
 
-    expect(isEmpty(undefined)).toBe(true);
-    expect(t1).toEqual([1, 2, "foo"]);
+    expect(isDefined(undefined)).toBe(false);
+    expect(t1).toEqual([1, 2, "foo", ""]);
 
     type cases = [
       Expect<Equal<typeof t1, [1, 2, string, ""]>>
