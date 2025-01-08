6761358161737bebc9460d13242d56fa9871fd9a
Ken Snyder <ken@ken.net>
2025-01-08 12:23:02 -0800

    feat: added filterUndefined() utility

--- modules/runtime/src/lists/index.ts
+++ modules/runtime/src/lists/index.ts
@@ -1,15 +1,9 @@
-// #autoindex
-
-// #region auto-indexed files
-// index last changed at: 8th Aug, 2022, 09:51 AM ( GMT-7 )
-// hash-code: bf4a6791
-
-// file exports
 export * from "./asArray";
 export * from "./createConverter";
 export * from "./createFixedLengthArray";
 export * from "./filter";
 export * from "./filterEmpty";
+export * from "./filterUndefined";
 export * from "./find";
 export * from "./getEach";
 export * from "./indexOf";
@@ -21,8 +15,3 @@ export * from "./reverse";
 export * from "./shift";
 export * from "./slice";
 export * from "./unique";
-
-// #endregion auto-indexed files
-
-// see https://github.com/inocan-group/do-devops/docs/autoindex.md
-// for more info
