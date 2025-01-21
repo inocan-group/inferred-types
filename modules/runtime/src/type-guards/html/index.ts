285df454fbcbfcadbf6a9bb05d1543540a3cd02f
Ken Snyder <ken@ken.net>
2025-01-21 12:48:42 -0800

    feat: added is HtmlComponentTag() type guard

--- modules/runtime/src/type-guards/html/index.ts
+++ modules/runtime/src/type-guards/html/index.ts
@@ -1,4 +1,6 @@
+export * from "./component-tags";
 export * from "./hasHtml";
 export * from "./html-tags";
 export * from "./isHtml";
 export * from "./isHtmlElement";
+export * from "./validHtmlAttributes";
