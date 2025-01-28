e71f62dfc6c4bc6d7d26a3eb3fd8e5dd7a1d5717
Ken Snyder <ken@ken.net>
2025-01-27 17:15:36 -0800

    fix: improved isIp4Address() type guard after putting tests

--- modules/runtime/src/runtime-types/index.ts
+++ modules/runtime/src/runtime-types/index.ts
@@ -2,7 +2,6 @@ export * from "./asToken";
 export * from "./asType";
 export * from "./choices";
 export * from "./doesExtend";
-export * from "./ip6Prefix";
 export * from "./list";
 export * from "./shape";
 
