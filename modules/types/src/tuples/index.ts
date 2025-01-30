5553b7a724e288f9c5ae4911d9e6b29ce5dce349
Ken Snyder <ken@ken.net>
2025-01-30 09:57:32 -0800

    chore: removed some unused types

--- modules/types/src/tuples/index.ts
+++ modules/types/src/tuples/index.ts
@@ -1,19 +1,6 @@
-// #autoindex
-// #region auto-indexed files
-// index last changed at: 8th Aug, 2022, 09:51 AM ( GMT-7 )
-// hash-code: 582cd3f6
 
-// file exports
-export * from "./DictArray";
 export * from "./FirstKey";
-export * from "./FirstKeyValue";
 export * from "./FirstOfEach";
 export * from "./FixedLengthArray";
-export * from "./FromDictArray";
 export * from "./LastOfEach";
 export * from "./SecondOfEach";
-
-// #endregion auto-indexed files
-
-// see https://github.com/inocan-group/do-devops/docs/autoindex.md
-// for more info
