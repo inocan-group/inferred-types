298b9ffa3c2f69df7c0d2ad72044922df2b2915b
Ken Snyder <ken@ken.net>
2025-01-30 08:38:35 -0800

    feat: added NBSP branded type and runtime `nbsp` constant for non-breaking spaces

--- modules/runtime/src/dictionary/index.ts
+++ modules/runtime/src/dictionary/index.ts
@@ -1,10 +1,4 @@
-// #autoindex
-// #region auto-indexed files
-// index last changed at: 8th Aug, 2022, 09:51 AM ( GMT-7 )
-// hash-code: b0051c37
-
 export * from "./defineObject";
-// file exports
 export * from "./entries";
 export * from "./get";
 export * from "./keysOf";
@@ -12,6 +6,7 @@ export * from "./narrowObjectTo";
 export * from "./objectValues";
 export * from "./omit";
 export * from "./retain";
+export * from "./reverseLookup";
 export * from "./sharedKeys";
 export * from "./takeProp";
 export * from "./valuesOf";
@@ -20,8 +15,3 @@ export * from "./withKeys";
 export * from "./withoutKeys";
 export * from "./withoutValue";
 export * from "./withValue";
-
-// #endregion auto-indexed files
-
-// see https://github.com/inocan-group/do-devops/docs/autoindex.md
-// for more info
