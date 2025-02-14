eca9c75f7127dc18d16c1360397b54b239f183d9
Ken Snyder <ken@ken.net>
2025-02-14 15:30:44 -0800

    chore: small improvements to tokens before branching

--- tests/domains/tokens/createGrammar.test.ts
+++ tests/domains/tokens/createGrammar.test.ts
@@ -1,4 +1,4 @@
-import { } from "@type-challenges/utils";
+import { Equal, Expect } from "@type-challenges/utils";
 import {
   asUnion,
   createGrammar,
@@ -10,31 +10,32 @@ import {
   isTrue,
   isUndefined
 } from "inferred-types/runtime";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("createGrammar()", () => {
 
   // static token
-  const Null = createToken("null", "none")("null", isNull);
-  const Undefined = createToken("undefined", "none")("undefined", isUndefined);
-  const True = createToken("true", "none")("true", isTrue);
+  const Null = createToken("null", "static")("null", isNull);
+  const Undefined = createToken("undefined", "static")("undefined", isUndefined);
+  const True = createToken("true", "static")("true", isTrue);
   // dynamic token
-  const Str = createToken("string", "any")(
+  const Str = createToken("string", "dynamic")(
     // type renderer
-    (p, sep) => {
-      const type = p.length === 0
-        ? "string" as string
-        : asUnion(p, sep, { prefix: `string${sep}` });
-      const typeGuard = p.length === 0
-        ? isString
-        : (val: unknown): val is typeof type => {
-          return isString(val) && p.includes(val);
-        }
-      return {
-        type,
-        typeGuard
-      } as any;
-    },
+    ({ sep }) =>
+      (...p) => {
+        const type = p.length === 0
+          ? "string" as string
+          : asUnion(p, sep, { prefix: `string${sep}` });
+        const typeGuard = p.length === 0
+          ? isString
+          : (val: unknown): val is typeof type => {
+            return isString(val) && p.includes(val);
+          }
+        return {
+          type,
+          typeGuard
+        } as any;
+      },
     // tokenizer
     (...literals) => eachAsString(...literals)
   );
@@ -46,13 +47,27 @@ describe("createGrammar()", () => {
     "::"
   );
 
+  const g = createGrammar(Syntax, Null, Undefined, True, Str);
 
-  it("happy path", () => {
-    const g = createGrammar(Syntax, Null, Undefined, True, Str)
+  it("token names are set correctly", () => {
+    const names = g.tokenNames;
 
+    expect(names).toEqual(["null", "undefined", "true", "string"])
 
     type cases = [
-      /** type tests */
+      Expect<Equal<typeof names, ["null", "undefined", "true", "string"]>>,
     ];
+  });
+
+  it("tokenShape is correct", () => {
+    const shaper = g.tokenShape;
+    const all = g.tokenShape();
+    const nada = g.tokenShape(Null);
+    const str = g.tokenShape(Str);
+
+    type cases = [
+      Expect<Equal<typeof nada, `{{null}}`>>,
+    ];
+
   });
 });
