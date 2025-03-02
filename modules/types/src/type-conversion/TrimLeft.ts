a6c881eeabc54819b5678187204cc06d14b8949b
Ken Snyder <ken@ken.net>
2025-03-02 09:26:08 -0800

    feat: added TrimStart and TrimEnd which are aliased to TrimLeft and TrimRight

--- modules/types/src/type-conversion/TrimLeft.ts
+++ modules/types/src/type-conversion/TrimLeft.ts
@@ -12,3 +12,15 @@ import type { Whitespace } from "inferred-types/types";
 export type TrimLeft<S extends string> = string extends S ? string :
     S extends `${Whitespace}${infer Right}` ?
         TrimLeft<Right> : S;
+
+
+/**
+ * Trims off whitespace at the start of the string
+ * ```ts
+ * // "foobar "
+ * type T = TrimStart<"\n\t  foobar ">;
+ * // string
+ * type T = TrimStart<string>;
+ * ```
+ */
+export type TrimStart<S extends string> = TrimLeft<S>;
