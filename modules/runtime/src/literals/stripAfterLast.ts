8f6665490dd3a1012ae499fa31d29371a0bd4172
Ken Snyder <ken@ken.net>
2025-03-09 12:02:50 -0700

    feat: SplitAfterLast<T,U> and splitAfterLast(content,split) utilities added

---
+++ modules/runtime/src/literals/stripAfterLast.ts
@@ -0,0 +1,21 @@
+import type { StripAfterLast } from "inferred-types/types";
+import { split } from "inferred-types/runtime";
+
+/**
+ * **stripAfterLast**(content, find)
+ *
+ * Runtime utility which strips out all characters after (and including)
+ * the _last_ instance of the "find" string while returning a strong type.
+ *
+ * ### Example
+ * ```ts
+ * // "hello world"
+ * const hello = stripAfterLast("hello world Joey", " ");
+ * ```
+ */
+export function stripAfterLast<
+    TContent extends string,
+    TBreak extends string,
+>(content: TContent, find: TBreak) {
+    return split.inline(content, find).slice(0,-2).join("") as unknown as StripAfterLast<TContent, TBreak>;
+}
