d1d00d8fd4e428c269024a31912f6cfd914fb38a
Ken Snyder <ken@ken.net>
2025-04-29 08:53:18 -0700

    feat: added Shortest<T> and Longest<T> type utilities

---
+++ tests/lists/Longest.ts
@@ -0,0 +1,48 @@
+import { describe, it } from "vitest";
+import {
+    Expect,
+    Longest,
+    Test,
+} from "inferred-types/types";
+
+describe("Longest<T>", () => {
+
+    it("happy path", () => {
+        type ClearWinner = Longest<[
+            "foobar",
+            "foobarbaz",
+            "foo",
+            "bart"
+        ]>;
+        type Tie = Longest<[
+            "bar",
+            "Robert",
+            "purple",
+            "foo",
+        ]>;
+        type Tie2 = Longest<[
+            "bar",
+            "purple",
+            "Robert",
+            "foo",
+        ]>;
+
+        type Wide = Longest<[string, string]>;
+
+        type SomeWide = Longest<[
+            "Hello",
+            string,
+            "Robert"
+        ]>
+
+        type cases = [
+            Expect<Test<ClearWinner, "equals", "foobarbaz">>,
+            Expect<Test<Tie, "equals", "Robert">>,
+            Expect<Test<Tie2, "equals", "purple">>,
+
+            Expect<Test<Wide, "equals", string>>,
+            Expect<Test<SomeWide, "equals", "Robert">>,
+        ];
+    });
+
+});
