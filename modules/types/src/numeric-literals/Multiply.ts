af26fc1bc917e78bacc9ed71f2b65c3a09fbbc7d
Ken Snyder <ken@ken.net>
2025-06-13 14:19:47 -0700

    feat: added Multiply<A,B> type utility

---
+++ modules/types/src/numeric-literals/Multiply.ts
@@ -0,0 +1,28 @@
+import { AfterFirst, AsNumber, Extends, FixedLengthArray, NumberLike, Or } from "inferred-types/types";
+
+/**
+ * **Multiply**`<A,B>`
+ *
+ * Multiplies two numbers together. Note, that these numbers must
+ * be integers.
+ */
+export type Multiply<
+    A extends NumberLike,
+    B extends NumberLike,
+
+    U extends readonly unknown[] = FixedLengthArray<1, AsNumber<A>>,
+    R extends readonly unknown[] = FixedLengthArray<1, AsNumber<B>>,
+    S extends readonly unknown[] = []
+> = Or<[Extends<AsNumber<A>, 0>, Extends<AsNumber<B>, 0>]> extends true
+? 0
+: [] extends R
+? S["length"]
+: Multiply<
+    A,B,
+    U,
+    AfterFirst<R>,
+    [
+        ...S, ...U
+    ]
+>
+
