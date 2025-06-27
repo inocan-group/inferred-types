6feab0d428a3b374997345b7c474cd9fed95a750
Ken Snyder <ken@ken.net>
2025-06-26 18:01:22 -0700

    chore: remove files creating clutter

--- benches/EnsureLeading.bench.ts
+++
@@ -1,9 +0,0 @@
-import { bench } from "@arktype/attest";
-import { EnsureLeading } from "../dist/types/index";
-
-export const test = () => {
-  bench("EnsureLeading<hi,'('>", () => {
-    return {} as EnsureLeading<"hi", "(">;
-  }).types([20, "instantiations"]);
-
-}
--- benches/Keys.bench.ts
+++
@@ -1,8 +0,0 @@
-import { bench } from "@arktype/attest";
-import { Keys } from "../dist/types/index";
-
-export const test = () => {
-  bench(`Keys<{foo: 1; bar: 2; baz: 3}>`, () => {
-    return {} as Keys<{foo: 1; bar: 2; baz: 3}>;
-  }).types([768, "instantiations"]);
-}
