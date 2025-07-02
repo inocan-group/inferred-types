4c21d19c4067da521252226ff3d90bf01c570dde
Ken Snyder <ken@ken.net>
2025-07-01 18:54:39 -0700

    fix: right the syntax used in the Compare.test.ts file

--- tests/boolean-logic/combinators/Compare.test.ts
+++ tests/boolean-logic/combinators/Compare.test.ts
@@ -11,8 +11,7 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
 
     it("equals", () => {
-        type T1 = Compare<42, "equals", 42>;
-        type T2 = Compare<42, "equals", [42]>;
+        type T1 = Compare<42, "equals", [42]>;
         type T3 = Compare<"foo", "equals", ["foo"]>;
 
         type B1 = Compare<string, "equals", ["foo"]>;
@@ -27,7 +26,6 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
-            Expect<Test<T2, "equals",  true>>,
             Expect<Test<T3, "equals",  true>>,
 
             Expect<Test<B1, "equals",  boolean>>,
@@ -44,13 +42,11 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
 
     it("extends", () => {
-        type T1 = Compare<42, "extends", number>;
-        type T1a = Compare<42, "extends", [number]>;
+        type T1 = Compare<42, "extends", [number]>;
         type T1x = Compare<42, "extends">;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
-            Expect<Test<T1a, "equals",  true>>,
             Expect<Test<T1x, "extends",  Error>>,
         ];
     });
@@ -61,45 +57,38 @@ describe("Compare<TVal,TOp,TComparator", () => {
         type T2 = Compare<99, "equalsSome", [42, 99]>;
 
         // @ts-ignore
-        type E1 = Compare<42, "equalsSome", 42>; // Error
-        // @ts-ignore
-        type E2 = Compare<42, "equalsSome", [42]>; // Error
+        type E1 = Compare<42, "equalsSome", [42]>; // Error
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
             Expect<Test<T2, "equals",  true>>,
-            Expect<Test<E1, "isError", "invalid-parameters">>,
-            Expect<Test<E2, "isError", "invalid-parameters">>,
+            Expect<Test<E1, "isError", "invalid-parameters">>
         ];
     });
 
 
 
     it("startsWith", () => {
-        type T1 = Compare<420, "startsWith", 42>;
-        type T2 = Compare<"foobar", "startsWith", "foo">;
+        type T1 = Compare<420, "startsWith", [42]>;
+        type T2 = Compare<"foobar", "startsWith", ["foo"]>;
         type T3 = Compare<"Foo", "startsWith", [UpperAlphaChar]>;
-        type T4 = Compare<"Foo", "startsWith", UpperAlphaChar>;
 
-        type F1 = Compare<"foo", "startsWith", UpperAlphaChar>;
-        type F2 = Compare<"foo", "startsWith", [UpperAlphaChar]>;
+        type F1 = Compare<"foo", "startsWith", [UpperAlphaChar]>;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
             Expect<Test<T2, "equals",  true>>,
             Expect<Test<T3, "equals",  true>>,
-            Expect<Test<T4, "equals",  true>>,
 
             Expect<Test<F1, "equals",  false>>,
-            Expect<Test<F2, "equals",  false>>,
         ];
     });
 
     it("greaterThan", () => {
-        type T1 = Compare<42, "greaterThan", 30>;
-        type T2 = Compare<"42", "greaterThan", 30>;
-        type F1 = Compare<"42", "greaterThan", 42>;
-        type B1 = Compare<number, "greaterThan", 42>;
+        type T1 = Compare<42, "greaterThan", [30]>;
+        type T2 = Compare<"42", "greaterThan", [30]>;
+        type F1 = Compare<"42", "greaterThan", [42]>;
+        type B1 = Compare<number, "greaterThan", [42]>;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
@@ -113,10 +102,10 @@ describe("Compare<TVal,TOp,TComparator", () => {
         type T1 = Compare<false, "false">;
         type T2 = Compare<true, "true">;
         type T3 = Compare<"true", "truthy">;
-        type T4 = Compare<"false", "falsy">;
+        type T4 = Compare<"", "falsy">;
 
         type F1 = Compare<"true", "true">;
-        type F2 = Compare<"false", "false">;
+        type F2 = Compare<"", "false">;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
@@ -131,12 +120,12 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
 
     it("contains with string input", () => {
-        type T1 = Compare<"foobar", "contains", "foo">;
+        type T1 = Compare<"foobar", "contains", ["foo"]>;
 
-        type B1 = Compare<string, "contains", "foo">;
-        type B2 = Compare<"foo", "contains", string>;
+        type B1 = Compare<string, "contains", ["foo"]>;
+        type B2 = Compare<"foo", "contains", [string]>;
 
-        type F1 = Compare<"foobar", "contains", "bax">;
+        type F1 = Compare<"foobar", "contains", ["bax"]>;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
@@ -148,15 +137,15 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
     it("contains with tuple input", () => {
         type Tup = ["foo","bar",42];
-        type T1 = Compare<Tup, "contains", "foo">;
-        type T2 = Compare<Tup, "contains", string>;
-        type T3 = Compare<Tup, "contains", number>;
+        type T1 = Compare<Tup, "contains", ["foo"]>;
+        type T2 = Compare<Tup, "contains", [string]>;
+        type T3 = Compare<Tup, "contains", [number]>;
         type T3a = Contains<Tup, number>;
-        type T4 = Compare<Tup, "contains", 42>;
+        type T4 = Compare<Tup, "contains", [42]>;
         type T4a = Contains<Tup, 42>;
 
-        type F1 = Compare<Tup, "contains", "bax">;
-        type F2 = Compare<Tup, "contains", 99>;
+        type F1 = Compare<Tup, "contains", ["bax"]>;
+        type F2 = Compare<Tup, "contains", [99]>;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
@@ -175,9 +164,9 @@ describe("Compare<TVal,TOp,TComparator", () => {
         type T1 = Compare<"foobar", "containsSome", ["foo", "bar"]>;
         type T2 = Compare<"foo", "containsSome", [string, number]>;
 
-        type B1 = Compare<string, "contains", "foo">;
+        type B1 = Compare<string, "contains", ["foo"]>;
 
-        type F1 = Compare<"foobar", "contains", "bax">;
+        type F1 = Compare<"foobar", "contains", ["bax"]>;
 
         type cases = [
             Expect<Test<T1, "equals",  true>>,
@@ -189,14 +178,14 @@ describe("Compare<TVal,TOp,TComparator", () => {
 
 
     it("numeric operators", () => {
-        type T1 = Compare<4, "greaterThan", 2>;
-        type T2 = Compare<2, "lessThan", 4>;
+        type T1 = Compare<4, "greaterThan", [2]>;
+        type T2 = Compare<2, "lessThan", [4]>;
 
-        type T3 = Compare<4, "greaterThanOrEqual", 2>;
-        type T4 = Compare<4, "greaterThanOrEqual", 4>;
+        type T3 = Compare<4, "greaterThanOrEqual", [2]>;
+        type T4 = Compare<4, "greaterThanOrEqual", [4]>;
 
-        type T5 = Compare<2, "lessThanOrEqual", 4>;
-        type T6 = Compare<4, "lessThanOrEqual", 4>;
+        type T5 = Compare<2, "lessThanOrEqual", [4]>;
+        type T6 = Compare<4, "lessThanOrEqual", [4]>;
 
         type T7 = Compare<5, "betweenInclusively", [1,10]>;
         type T8 = Compare<5, "betweenExclusively", [1,10]>;
