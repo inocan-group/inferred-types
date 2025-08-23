13d5d99fa966368b8a6fe8cc190cb1ab13b7d63e
Ken Snyder <ken@ken.net>
2025-08-23 07:54:35 -0700

    feat: added TemplateMap<T>, TemplateMap__Basic, TemplateMap__Generics

---
+++ modules/types/src/interpolation/template-maps.ts
@@ -0,0 +1,31 @@
+import { DefineObject, EmptyObject, ExpandRecursively, GenericParam, MergeObjects } from "inferred-types/types";
+
+
+export type TemplateMap__Basic = {
+    string: "string";
+    number: "number";
+    boolean: "boolean";
+}
+
+/**
+ * **TemplateMap__Generics**`<T>`
+ *
+ * Provides a static template based on the generic types provided. It also
+ * always includes the basics for `string`, `number`, and `boolean`.
+ */
+export type TemplateMap__Generics<
+    T extends readonly GenericParam[],
+    G extends DefineObject = TemplateMap__Basic
+> = T extends [ infer Head extends GenericParam, ...infer Rest extends readonly GenericParam[]]
+    ? TemplateMap__Generics<Rest, G & Record<Head["name"], Head["token"]>>
+: ExpandRecursively<G>;
+
+
+/**
+ * **TemplateMap**`<[T]>`
+ *
+ * By default it provides the basics of _string_, _number_, and _boolean_ but when
+ * you set the generic `T` using a `DefineObject` this map will be combined with the
+ * basics.
+ */
+export type TemplateMap<T extends DefineObject = EmptyObject> = MergeObjects<TemplateMap__Basic, T>;
