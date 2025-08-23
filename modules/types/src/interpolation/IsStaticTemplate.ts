1bb670050920f8030a3ca2e45e5aca900d4066d3
Ken Snyder <ken@ken.net>
2025-08-23 07:50:24 -0700

    feat: added IsStaticTemplate<T> boolean operator

---
+++ modules/types/src/interpolation/IsStaticTemplate.ts
@@ -0,0 +1,27 @@
+import { Contains } from "inferred-types/types";
+import { StringKeys } from "types/dictionary";
+import { TemplateMap__Basic } from "types/interpolation/template-maps";
+import { DefineObject } from "types/runtime-types";
+
+
+type Curly<
+    T extends DefineObject,
+    K extends readonly string[] = StringKeys<T>,
+    O extends string = never
+> = K extends [ infer Head extends string, ...infer Rest extends readonly string[]]
+    ? Curly<T,Rest,O | `{{${Head}}}`>
+: O;
+
+
+/**
+ * **IsStaticTemplate**`<TContent,[TSegments]>`
+ *
+ * A boolean operator which indicates whether any of the dynamic segments
+ * defined in `TSegments` was found in `TContent`.
+ */
+export type IsStaticTemplate<
+    TContent extends string,
+    TSegments extends DefineObject = TemplateMap__Basic
+> = Contains<TContent, Curly<TSegments>>;
+
+
