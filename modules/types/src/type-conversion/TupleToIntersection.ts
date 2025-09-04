9f82e5d1adc753b4a725aa9c591044919a76138f
Ken Snyder <ken@ken.net>
2025-09-04 15:23:33 -0700

    feat: improved TupleToIntersection to produce nicer looking types and to report errors if instructed to

--- modules/types/src/type-conversion/TupleToIntersection.ts
+++ modules/types/src/type-conversion/TupleToIntersection.ts
@@ -1,10 +1,74 @@
+import { Dictionary, Err, ExpandRecursively, FnKeyValue, IsNever, IsUnset, RemoveFnProps, TypedFunction, Unset } from "inferred-types/types";
+
+type Break<
+    TMsg extends string,
+    TCtx extends Dictionary,
+    E extends "error" | "never"
+> = E extends "error"
+? Err<"invalid-intersection", TMsg, TCtx>
+: never;
+
+type Set<V, W> = IsUnset<V> extends true
+    ? W
+    : V & W;
+
+;
+
+type Intersect<
+    T extends readonly unknown[],
+    E extends "error" | "never",
+    F extends TypedFunction | undefined = undefined,
+    V = Unset
+> = T extends [infer Head, ...infer Rest]
+    ? Head extends TypedFunction
+        ? IsNever<Set<V, FnKeyValue<Head>>> extends true
+            ? Break<
+                `A function with a key-value dictionary caused the intersection type to become 'never'`,
+                { intersection: V,  conflict: Head},
+                E
+            >
+            : Intersect<
+                Rest,
+                E,
+                Head,
+                Set<V, FnKeyValue<Head>>
+            >
+    : IsNever<Set<V,Head>> extends true
+        ? Break<
+            `TupleToIntersection<T> hit an element which caused the intersection type to become 'never'`,
+            { intersection: V, conflict: Head },
+            E
+        >
+        : Intersect<
+        Rest,
+        E,
+        F,
+        Set<V,Head>
+    >
+
+
+: F extends undefined
+    ? ExpandRecursively<V>
+: F extends TypedFunction
+    ? F & ExpandRecursively<V>
+: never;
+
+
 /**
+ * **TupleToIntersection**`<T, [E]>`
+ *
  * Recursively intersects all the elements of a tuple.
  *
- * - If T is [H, ...R], result = H & TupleToIntersection<R>
- * - Otherwise (empty tuple), result = unknown (the identity for intersection)
+ * - Empty tuples will return `unknown` as type
+ * - if you want an Error instead of the `never` type you can set `E` to
+ * "error" and the conflicting element which _caused_ the intersection to
+ * become `never` will be highlighted.
  */
-export type TupleToIntersection<T extends readonly unknown[]>
-  = T extends [infer Head, ...infer Rest]
-      ? Head & TupleToIntersection<Rest>
-      : unknown;
+export type TupleToIntersection<
+    T extends readonly unknown[],
+    E extends "error" | "never" = "never"
+> = [] extends T
+? unknown
+: Intersect<T, E>;
+
+
---
+++ tests/type-conversions/TupleToIntersection.test.ts
@@ -0,0 +1,72 @@
+import { describe, it } from "vitest";
+import {
+    Expect,
+    Test,
+    TupleToIntersection,
+} from "inferred-types/types";
+
+describe("TupleToIntersection<T,[E]>", () => {
+
+    it("non-overlapping dictionaries", () => {
+        type T1 = TupleToIntersection<[{foo:1},{bar:2},{baz:3}]>;
+        type T2 = TupleToIntersection<[{foo:1},{bar:string},{baz:number}]>;
+
+        type cases = [
+            Expect<Test<T1, "equals", { foo: 1; bar: 2; baz: 3 }>>,
+            Expect<Test<T2, "equals", { foo: 1; bar: string; baz: number }>>,
+        ];
+    });
+
+
+    it("overlapping but non-destructive dictionaries", () => {
+        type T1 = TupleToIntersection<[{ foo: 1 }, { foo: number }]>;
+
+        type cases = [
+            Expect<Test<T1, "equals", { foo: 1 }>>
+        ];
+    });
+
+
+    it("overlapping and destructive dictionaries", () => {
+        type T1 = TupleToIntersection<[{ foo: 1 }, { foo: string }]>;
+        type E1 = TupleToIntersection<[{ foo: 1 }, { foo: string }], "error">;
+
+        type cases = [
+            Expect<Test<T1, "equals", never>>,
+
+            Expect<Test<E1, "isError", "invalid-intersection">>,
+            Expect<Test<E1["conflict"], "equals", { foo: string } >>
+        ];
+    });
+
+
+    it("dictionaries and functions intersected", () => {
+        type T1 = TupleToIntersection<[
+            (name: string) => "hi",
+            { foo: 1 },
+            { bar: 2 }
+        ]>;
+        type T2 = TupleToIntersection<[
+            ((name: string) => "hi") & { name: "greet"},
+            { foo: 1 },
+            { bar: 2 }
+        ]>
+
+
+        type cases = [
+            Expect<Test<T1, "equals", ((name: string) => "hi") & {
+                foo: 1;
+                bar: 2;
+            }>>,
+            Expect<Test<T2, "equals", ((name: string) => "hi") & {
+                name: "greet";
+                foo: 1;
+                bar: 2;
+            }>>,
+        ];
+    });
+
+
+
+
+});
