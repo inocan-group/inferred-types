0b342c82245e3d17a172db75f9797eed8dbb7790
Ken Snyder <ken@ken.net>
2025-09-10 09:29:43 -0700

    fix: fixed edge cases for IT_TakeNumericLiteral so that it can better detect boundary conditions

--- modules/types/src/runtime-types/type-defn/input-tokens/IT_TakeNumericLiteral.ts
+++ modules/types/src/runtime-types/type-defn/input-tokens/IT_TakeNumericLiteral.ts
@@ -1,31 +1,44 @@
 import type {
     As,
+    AsNumber,
+    Contains,
     Err,
-    ErrType,
-    IsInputTokenSuccess,
     IT_TakeOutcome,
     IT_Token,
     NumericChar,
-    RetainWhile,
+    RetainUntil,
     StartsWith,
-    StripWhile,
-    Trim
+    StripLeading,
+    Trim,
+    Whitespace
 } from "inferred-types/types";
 
-type Bare<T extends string> = StartsWith<T, NumericChar> extends true
-    ? [RetainWhile<T, NumericChar>, StripWhile<T, NumericChar>] extends [
-        `${infer Num extends number}`,
-        infer Rest extends string
-    ]
+type Break = Whitespace | "|" | "&";
+
+type TakeNumber<
+    T extends string,
+    R extends string = ''
+> = T extends `${infer Head}${infer Rest}`
+    ? Head extends Break
+        ? R
+    : TakeNumber<Rest, `${R}${Head}`>
+: R;
+
+
+
+type Bare<T extends string> = StartsWith<T, NumericChar|"-"> extends true
+    ? TakeNumber<T> extends `${number}`
         ? {
             __kind: "IT_Token";
             kind: "literal";
-            token: `${Num}`;
-            type: Num;
-            rest: Trim<Rest>;
+            token: `${TakeNumber<T>}`;
+            type: AsNumber<TakeNumber<T>>;
+            rest: Trim<StripLeading<T, TakeNumber<T>>>;
         }
         : Err<
-            `wrong-handler`
+            `malformed-token/numeric-literal`,
+            `A bare number was detected but the resultant token of '${TakeNumber<T>}' can not be converted to a number!`,
+            { parse: T; token: TakeNumber<T> }
         >
     : Err<
         `wrong-handler/numeric-literal`,
@@ -33,8 +46,9 @@ type Bare<T extends string> = StartsWith<T, NumericChar> extends true
         { token: T }
     >;
 
-type NumberConstructor<T extends string>
-    = T extends `Number(${infer Num extends number})${infer Rest extends string}`
+
+type NumberConstructor<T extends string> =
+T extends `Number(${infer Num extends number})${infer Rest extends string}`
         ? {
             __kind: "IT_Token";
             kind: "literal";
@@ -42,17 +56,37 @@ type NumberConstructor<T extends string>
             type: Num;
             rest: Trim<Rest>;
         }
+: T extends `Number(${infer Rest extends string}`
+    ? Contains<Rest, ")"> extends true
+        ? Err<
+            `malformed-token/numeric-literal`,
+            `A numeric literal with the 'Number(defn)' syntax provided an invalid numeric value of '${RetainUntil<Rest,")">}'!`,
+            { token: T }
+        >
+        : Err<
+            `malformed-token/numeric-literal`,
+            `A numeric literal definition was detected by the 'Numeric(' but no terminating ')' character found!`,
+            { token: T }
+        >
+: Err<"wrong-handler/numeric-literal">;
 
-        : Err<"wrong-handler/numeric-literal">;
 
+/**
+ * **IT_TakeNumericLiteral**`<T>`
+ *
+ * Attempts to parse a numeric literal from the head of the parse string.
+ */
 export type IT_TakeNumericLiteral<T extends string> = As<
-    IsInputTokenSuccess<Bare<T>> extends true
-        ? Bare<T> extends infer Success extends IT_Token<"literal">
-            ? Success
-            : never
-        : ErrType<Bare<T>> extends "malformed-token"
-            ? Bare<T>
-            : NumberConstructor<T>,
-
+    Bare<T> extends IT_Token<"literal">
+        ? Bare<T>
+    : NumberConstructor<T> extends IT_Token<"literal">
+        ? NumberConstructor<T>
+    : Bare<T> extends Err<"malformed-token">
+        ? Bare<T>
+    : NumberConstructor<T> extends Err<"malformed-token">
+        ? NumberConstructor<T>
+    : Err<"wrong-handler/numeric-literal">
+    ,
     IT_TakeOutcome<"literal">
 >;
+
--- tests/runtime/input-tokens/IT_TakeNumericLiteral.test.ts
+++ tests/runtime/input-tokens/IT_TakeNumericLiteral.test.ts
@@ -1,33 +1,90 @@
 import { describe, it } from "vitest";
 import {
     Expect,
-    IT_TakeStringLiteral,
+    IT_TakeNumericLiteral,
+    IT_Token,
     Test,
 } from "inferred-types/types";
-import { IT_TakeNumericLiteral } from "types/runtime-types/type-defn/input-tokens/IT_TakeNumericLiteral";
 
 describe("IT_TakeNumericLiteral<T>", () => {
 
     it("bare numeric literal", () => {
-        type Num = IT_TakeNumericLiteral<"42">;
-        type NumRemaining = IT_TakeNumericLiteral<"42 | 56">;
+        type Complete = IT_TakeNumericLiteral<"42">;
+        type Partial = IT_TakeNumericLiteral<"42 | 56">;
+
+        type Invalid = IT_TakeNumericLiteral<"42a">;
+
 
         type cases = [
-            Expect<Test<Num["type"], "equals", 42>>,
-            Expect<Test<NumRemaining["type"], "equals", 42>>,
-            Expect<Test<NumRemaining["rest"], "equals", "| 56">>,
+            Expect<Test<Complete, "extends", IT_Token<"literal">>>,
+            Expect<Test<Invalid, "isError", "malformed-token/numeric-literal">>,
+
+            Expect<Test<Complete["type"], "equals", 42>>,
+            Expect<Test<Partial["type"], "equals", 42>>,
+            Expect<Test<Partial["rest"], "equals", "| 56">>,
+        ];
+    });
+
+    it("negative number", () => {
+        type Complete = IT_TakeNumericLiteral<"-42">;
+        type Partial = IT_TakeNumericLiteral<"-42 | 56">;
+
+        type cases = [
+            Expect<Test<Complete, "extends", IT_Token<"literal">>>,
+
+            Expect<Test<Complete["type"], "equals", -42>>,
+            Expect<Test<Partial["type"], "equals", -42>>,
+            Expect<Test<Partial["rest"], "equals", "| 56">>,
         ];
     });
 
 
-    it("Numeric constructor", () => {
-        type Num = IT_TakeNumericLiteral<"Number(42)">;
-        type NumRemaining = IT_TakeNumericLiteral<"Number(42) | Number(56)">;
+    it("float", () => {
+        type Complete = IT_TakeNumericLiteral<"42.01">;
+        type Partial = IT_TakeNumericLiteral<"42.01 | 56">;
 
         type cases = [
-            Expect<Test<Num["type"], "equals", 42>>,
-            Expect<Test<NumRemaining["type"], "equals", 42>>,
-            Expect<Test<NumRemaining["rest"], "equals", "| Number(56)">>,
+            Expect<Test<Complete, "extends", IT_Token<"literal">>>,
+
+            Expect<Test<Complete["type"], "equals", 42.01>>,
+            Expect<Test<Partial["type"], "equals", 42.01>>,
+            Expect<Test<Partial["rest"], "equals", "| 56">>,
+        ];
+    });
+
+
+
+    it("Numeric constructor", () => {
+        type CompleteInt = IT_TakeNumericLiteral<"Number(42)">;
+        type PartialInt = IT_TakeNumericLiteral<"Number(42) | Number(56)">;
+
+        type CompleteFloat = IT_TakeNumericLiteral<"Number(42.01)">;
+        type PartialFloat = IT_TakeNumericLiteral<"Number(42.01) | Number(56)">;
+
+        type NegCompleteFloat = IT_TakeNumericLiteral<"Number(-42.01)">;
+        type NegPartialFloat = IT_TakeNumericLiteral<"Number(-42.01) | Number(56)">;
+
+        type Invalid = IT_TakeNumericLiteral<"Number(abc)">;
+
+        type cases = [
+            Expect<Test<CompleteInt, "extends", IT_Token<"literal">>>,
+            Expect<Test<PartialInt, "extends", IT_Token<"literal">>>,
+            Expect<Test<CompleteFloat, "extends", IT_Token<"literal">>>,
+            Expect<Test<PartialFloat, "extends", IT_Token<"literal">>>,
+            Expect<Test<Invalid, "isError", "malformed-token">>,
+
+            Expect<Test<CompleteInt["type"], "equals", 42>>,
+            Expect<Test<PartialInt["type"], "equals", 42>>,
+            Expect<Test<PartialInt["rest"], "equals", "| Number(56)">>,
+
+            Expect<Test<CompleteFloat["type"], "equals", 42.01>>,
+            Expect<Test<PartialFloat["type"], "equals", 42.01>>,
+            Expect<Test<PartialFloat["rest"], "equals", "| Number(56)">>,
+
+            Expect<Test<NegCompleteFloat["type"], "equals", -42.01>>,
+            Expect<Test<NegPartialFloat["type"], "equals", -42.01>>,
+            Expect<Test<NegPartialFloat["rest"], "equals", "| Number(56)">>,
+
         ];
     });
 
