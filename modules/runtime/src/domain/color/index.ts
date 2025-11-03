3ffcb5833b3b9f145e4066102bb00412236d86c0
Ken Snyder <ken@ken.net>
2025-11-02 19:24:44 -0800

    fix: export isRbgTuple() util

--- modules/runtime/src/domain/color/index.ts
+++ modules/runtime/src/domain/color/index.ts
@@ -6,3 +6,5 @@ export * from "./isCssRgbString";
 export * from "./isHexColor";
 export * from "./isRgbaObject";
 export * from "./isRgbObject";
+export * from "./isRgbTuple";
+export * from "./isRgbDecimal"
